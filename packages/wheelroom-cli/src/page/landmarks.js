f2411faea0076c1b4b45fb1bc0076a27b2d74072
Jacco Meijer <jaccom@emerson-hotel.local>
2019-05-19 17:53:35 +0200

    chore(main): move files into their own repository

---
+++ packages/wheelroom-cli/src/page/landmarks.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react'
+
+// This element wraps the landmark element specified in sections.jsx around each
+// section. Not that the landmarks are added in a fixed order as shown in the
+// Fragment below. As a consequence the order of the sections is bound to the
+// order of the landmarks below.
+//
+
+const Landmarks = props => {
+  const landMarkedChildren = {
+    aside: [],
+    footer: [],
+    header: [],
+    main: [],
+  }
+
+  React.Children.forEach(props.children, child => {
+    landMarkedChildren[child.props.landMark].push(child)
+  })
+
+  return (
+    <Fragment>
+      <header>{landMarkedChildren.header}</header>
+      <main>{landMarkedChildren.main}</main>
+      <aside>{landMarkedChildren.aside}</aside>
+      <footer>{landMarkedChildren.footer}</footer>
+    </Fragment>
+  )
+}
+
+export default Landmarks
