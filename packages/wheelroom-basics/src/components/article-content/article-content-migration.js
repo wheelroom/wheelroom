72cba6f579a5fc175e98420b0068e73a48eb98c9
Jacco Meijer <jaccom@emerson-hotel.local>
2019-05-27 19:08:37 +0200

    chore: cleanup

--- packages/wheelroom-basics/src/components/article-content/article-content-migration.js
+++
@@ -1,9 +0,0 @@
-const migrationHelper = require('../../plugins/gatsby-plugin-wheelroom/migration-helper/migration-helper')
-
-module.exports = function(migration, context) {
-  migrationHelper.doMigration({
-    migration,
-    context,
-    jsonPath: 'src/components/article-content.json',
-  })
-}
