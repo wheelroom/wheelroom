71d1e7f81685a064465ca66374d7b257a855623d
Jacco Meijer <jaccom@emerson-hotel.local>
2019-06-04 23:49:02 +0200

    chore: export components

--- packages/gatsby-theme-wheelroom/src/index.ts
+++ packages/gatsby-theme-wheelroom/src/index.ts
@@ -4,3 +4,7 @@ export * from './lib/page/get-meta-tags'
 export * from './lib/page/landmarks'
 export * from './lib/page/seo'
 export * from './lib/page/model'
+
+export * from './components/article-content/article-content-graphql'
+export * from './components/article-section/article-section-graphql'
+export * from './components/globals-part/globals-part-graphql'
