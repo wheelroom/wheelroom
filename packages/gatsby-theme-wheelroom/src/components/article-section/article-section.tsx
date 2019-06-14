e14697d00d9a12c13f959f5c23ac29b6ddb350ef
Jacco Meijer <info@jaccomeijer.nl>
2019-06-14 14:53:08 +0200

    chore: more demo styling

--- packages/gatsby-theme-wheelroom/src/components/article-section/article-section.tsx
+++ packages/gatsby-theme-wheelroom/src/components/article-section/article-section.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
+import { ALink } from '../../lib/styling/core-elements/a-link'
 import { Box, Flex } from '../../lib/styling/core-elements/grid'
 import { Heading } from '../../lib/styling/core-elements/heading'
+import {
+  paragraphStyle,
+  simpleLinkStyle,
+} from '../../lib/styling/typography/paragraph'
 import { title1Style } from '../../lib/styling/typography/title'
 
 export interface ArticleSectionProps {
@@ -17,12 +22,22 @@ export const ArticleSection = (props: ArticleSectionProps) => {
         <Heading {...title1Style}>Article section</Heading>
       </Flex>
 
-      <Flex>
+      <Flex {...paragraphStyle}>
         <Box color="white" bg="lightBlue" w={[1, 1 / 2]}>
-          <Box p={[4, 5]}>Landmakr: {props.landMark}</Box>
+          <Box p={[4, 5]}>
+            <p>Landmakr: {props.landMark}</p>
+          </Box>
         </Box>
         <Box color="white" bg="blue" w={[1, 1 / 2]}>
-          <Box p={[4, 5]}>Locale: {props.locale}</Box>
+          <Box p={[4, 5]}>
+            <p>Locale: {props.locale}</p>
+            <p>
+              More
+              <ALink {...simpleLinkStyle} href="#somewhere">
+                Here is a link
+              </ALink>
+            </p>
+          </Box>
         </Box>
       </Flex>
     </div>
--- packages/gatsby-theme-wheelroom/src/lib/styling/core-elements/a-link.tsx
+++ packages/gatsby-theme-wheelroom/src/lib/styling/core-elements/a-link.tsx
@@ -2,7 +2,7 @@
 import { jsx } from '@emotion/core'
 import { styledSystem } from '../styled-system/styled-system'
 
-export const Alink = (props: any) => (
+export const ALink = (props: any) => (
   <a
     children={props.children}
     css={styledSystem({ textDecoration: 'none', ...props })}
