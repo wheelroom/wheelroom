069c38c2742fba4c8d83d3f38cf9afe3fd8e55a7
Jacco Meijer <jaccom@emerson-hotel.local>
2019-06-16 22:29:21 +0200

    chore: cleanup

--- packages/@jacco-meijer/wheelroom/types/components-map.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from './model';
-export interface ComponentConfig {
-    fragment?: any;
-    model: Model;
-    query?: any;
-}
-export interface ComponentsMap {
-    [componentType: string]: ComponentConfig;
-}
--- packages/@jacco-meijer/wheelroom/types/router.d.ts
+++
@@ -1,6 +0,0 @@
-export interface NamedPaths {
-    [pathName: string]: {
-        path: string;
-        [localeName: string]: string;
-    };
-}
