03e2c187af8f8a82fcf691d8487d9409dabb0fd6
Jacco Meijer <jaccom@emerson-hotel.local>
2019-06-16 18:11:00 +0200

    chore: consistent console.log's

--- packages/styled-system/src/styled-system/styled-system.tsx
+++ packages/styled-system/src/styled-system/styled-system.tsx
@@ -5,7 +5,7 @@ export const styledSystem = (props: any) => (theme: any) => {
   // Within a lerna monorepo this is caused by having two versions of the
   // emotion package
   if (Object.entries(theme).length === 0) {
-    console.log('Theme not found, using default theme')
+    console.log(`Theme not found, using default theme`)
     theme = defaultTheme
   }
 
