b90a07719be031180536ec0b882adc3d70b22ca8
Jacco Meijer <info@jaccomeijer.nl>
2019-06-17 16:39:04 +0200

    chore: add READMEs

--- packages/styled-system/src/core-elements/text.tsx
+++ packages/styled-system/src/core-elements/text.tsx
@@ -3,8 +3,5 @@ import { jsx } from '@emotion/core'
 import { styledSystem } from '../styled-system/styled-system'
 
 export const Text = (props: any) => (
-  <p
-    children={props.children}
-    css={styledSystem({ textDecoration: 'none', ...props })}
-  />
+  <p children={props.children} css={styledSystem(props)} />
 )
