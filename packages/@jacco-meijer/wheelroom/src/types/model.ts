4661e555ff59266c765aceb1c8a959183ddde6cc
Jacco Meijer <jaccom@emerson-hotel.local>
2019-06-22 18:11:06 +0200

    docs: add info on wheelroom types

--- packages/@jacco-meijer/wheelroom/src/types/model.ts
+++ packages/@jacco-meijer/wheelroom/src/types/model.ts
@@ -40,7 +40,27 @@ export type widgetID =
   | 'radio'
   | 'richTextEditor'
 
-export type wheelroomType = 'page' | 'subPage' | 'section' | 'global'
+/**
+ * MODELS, QUERIES and FRAGMENTS
+ * - query: used by Gatsby to retrieve data when building static pages
+ * - fragment: a Graphql fragment that is part of a (page-) query
+ * - model: used to create models at the headless CMS (Contentful) and to create
+ *   initial content
+ *
+ * WHEELROOM TYPES
+ *
+ * All wheelroom types define a model, depending on the type a query and/or a
+ * query fragment are defined
+ *
+ * - page: defines a query for retrieving pages
+ * - subPage: defines a query for retrieving subPages and a fragment defining
+ *   fields
+ * - section: has a fragment defining fields
+ * - global: defines a query for retrieving globals and a fragment defining fields
+ * - part: has a fragment defining fields
+ *
+ */
+export type wheelroomType = 'page' | 'subPage' | 'section' | 'global' | 'part'
 
 export interface Field {
   /** Wheelroom content used to create a demo entry */
