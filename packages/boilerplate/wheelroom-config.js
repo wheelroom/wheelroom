0902f2d7cbf5beecc2858aa25bd3d133f74500d1
Jacco Meijer <jaccom@emerson-hotel.local>
2019-07-08 12:06:42 +0200

    chore: WIP Working towards new modular setup

--- packages/boilerplate/wheelroom-config.js
+++ packages/boilerplate/wheelroom-config.js
@@ -1,17 +1,62 @@
 path = require('path')
 
+// WIP Working towards new modular setup
 module.exports = {
   defaultLocale: 'nl',
   defaultComponentResolve: `@jacco-meijer/content-models`,
-  initialPageSection: 'articleSection',
-  localComponentsMap: path.resolve('./dist/component-configs/components-map.js'),
   componentTypes: {
     articleSection: {},
     myNewComponent: {
-      resolve: 'localComponentsMap',
+      resolve: './dist/component-configs/components-map.js',
     },
     page: {
       resolve: `@jacco-meijer/content-models`,
     },
   },
+  plugins: [
+    {
+      resolve: 'wheelroom-plugin-contentful',
+      options: {
+        initialPageSection: 'articleSection',
+      },
+    },
+    {
+      resolve: 'module:wheelroom-plugin-templates',
+      options: {
+        defaultTemplateResolve: `@jacco-meijer/content-models`,
+        templateSets: {
+          fragments: [
+            {
+              fileName: '%componentFilename%.ts',
+              resolve: '@jacco-meijer/content-models',
+              template: 'fragmentTemplate',
+              wheelroomTypes: ['subPage', 'section', 'global', 'part'],
+            },
+          ],
+          component: [
+            {
+              fileName: '%componentFilename%/%componentFilename%.ts',
+              resolve: './templates/component-template',
+              template: 'componentTemplate',
+              wheelroomTypes: ['subPage', 'section', 'global', 'part'],
+            },
+            {
+              fileName: '%componentFilename%/%componentFilename%-basic-var.ts',
+              resolve: './templates/component-basic-var-template',
+              template: 'componentBasicVarTemplate',
+              wheelroomTypes: ['subPage', 'section', 'global', 'part'],
+            },
+          ],
+          config: [
+            {
+              fileName: '%componentFilename%/graphql.ts',
+              resolve: './templates/graphql-template',
+              template: 'graphqlTemplate',
+              wheelroomTypes: ['subPage', 'section', 'global', 'part'],
+            },
+          ],
+        },
+      },
+    },
+  ],
 }
