deb87b1b2e0a5054b12b1da1c4119de60f5eee9a
Jacco Meijer <jaccom@emerson-hotel.local>
2019-07-15 19:19:50 +0200

    chore: export methods

--- packages/gatsby-theme-wheelroom/src/index.ts
+++ packages/gatsby-theme-wheelroom/src/index.ts
@@ -1,2 +1,6 @@
 // Exporting commands makes it a valid wheelroom plugin
 export const commands = []
+
+export * from './types/landmarks'
+export * from './types/named-paths'
+export * from './lib/get-named-paths'
