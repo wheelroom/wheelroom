4eadb1b29281d67b6dd3c179daa4af1cde237f8b
Jacco Meijer <info@jaccomeijer.nl>
2019-07-17 09:25:09 +0200

    chore: use plugin options instead of wheelroom options

--- packages/gatsby-theme-wheelroom/src/lib/locales.ts
+++ packages/gatsby-theme-wheelroom/src/lib/locales.ts
@@ -1,11 +1,11 @@
 import { Context } from '../types/context'
 
-const defaultLocale = 'en_US'
+const defaultLocale = 'en-US'
 
 export const getLocale = (page: any) => {
   return page.node_locale
 }
 
 export const getDefaultLocale = (context: Context): string => {
-  return context.wheelroomPluginOptions.defaultLocale || defaultLocale
+  return context.options.defaultLocale || defaultLocale
 }
