b6d07e3dc41ee7d70d9c4779be32e53f71ec8db1
Jacco Meijer <jacco@jaccomeijer.nl>
2019-09-05 13:11:45 +0200

    ci: add coverage

--- packages/@jacco-meijer/styled-system/jest.config.js
+++ packages/@jacco-meijer/styled-system/jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
   },
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/styled-system.ts',
+    '!**/node_modules/**',
+    '!<rootDir>/dist/**',
+    '!**/*.{int,unit}.test.{ts,tsx,js,jsx}',
+  ],
+  coverageReporters: ['lcov', 'text-summary'],
 }
