66d5111b0a49857c7e9aaadc11a00387d2b17d74
Jacco Meijer <jaccom@emerson-hotel.local>
2019-12-08 21:01:23 +0100

    chore: basic setup of new config

--- packages/boilerplate/wheelroom-config.js
+++ packages/boilerplate/wheelroom-config.js
@@ -1,61 +1,100 @@
 var questionSets = require('./wheelroom-question-sets')
 var templateSets = require('./wheelroom-template-sets')
 
-newConfig = {
-  articleSection: {
-    fields: {
-      articleText: {
-        localized: true,
-      },
-    },
-    variations: [
-      'Inline article text, specific to this section',
-      'Linked article text, derived from slug in url',
-    ],
-  },
-}
-
 module.exports = {
-  defaultComponentResolve: `@jacco-meijer/content-models`,
+  fieldDefaults: {
+    localized: false,
+    required: false,
+    type: 'shortText',
+  },
   components: {
-    article: {},
-    articleSection: {
-      options: {
-        localizedFields: ['articleText'],
-        variations: [
-          'Inline article text, specific to this section',
-          'Linked article text, derived from slug in url',
-        ],
+    page: {
+      fields: {
+        navigationTitle: { required: true },
+        image: { type: 'image' },
+        seoDescription: { maxLength: 155, required: true },
+        seoTitle: { required: true },
+      },
+      query: {
+        mainQuery: true,
+        type: 'page',
       },
     },
     globals: {
-      options: {
-        localizedFields: [
-          'addressLine1',
-          'addressLine2',
-          'emailAddress',
-          'heading',
-          'siteAuthor',
-          'siteDescription',
-          'siteKeywords',
-          'siteTitle',
-        ],
-        skipFields: ['githubUrl', 'linkedinUrl'],
+      fields: {
+        addressLine1: { required: true },
+        addressLine2: { required: true },
+        emailAddress: { required: true },
+        githubUrl: {},
+        heading: { required: true },
+        linkedinUrl: {},
+        phoneNumber: {},
+        siteAuthor: {},
+        siteDescription: {},
+        siteKeywords: { type: 'tags' },
+        siteTitle: {},
+      },
+      query: {
+        mainQuery: true,
+        type: 'global',
       },
     },
-    myNewComponent: {
-      resolve: 'dist:component-configs',
+    article: {
+      fields: {
+        articleText: { type: 'richtText' },
+        author: { required: true },
+        createdAt: { systemField: true },
+        date: { type: 'date', required: true },
+        heading: { required: true },
+        image: { type: 'image' },
+        subHeading: {},
+        updatedAt: { systemField: true },
+      },
+      query: {
+        limit: 10,
+        mainQuery: true,
+        type: 'subPage',
+      },
     },
-    page: {
-      resolve: `@jacco-meijer/content-models`,
-      options: {
-        initialPageSection: 'articleSection',
-        localizedFields: [
-          'navigationTitle',
-          'path',
-          'seoDescription',
-          'seoTitle',
-        ],
+    articleSection: {
+      fields: {
+        articleText: {
+          localized: true,
+        },
+      },
+      variations: [
+        'Inline article text, specific to this section',
+        'Linked article text, derived from slug in url',
+      ],
+      query: {
+        mainQuery: false,
+        type: 'sectionOfPage',
+      },
+    },
+    articlesSection: {
+      fields: {
+        articles: {
+          type: 'multipleComponents',
+          components: ['article'],
+        },
+        heading: {},
+        maxArticles: {
+          type: 'number',
+          initialContent: 5,
+        },
+      },
+      query: {
+        mainQuery: false,
+        type: 'sectionOfPage',
+      },
+    },
+    examplePart: {
+      fields: {
+        someText: {},
+      },
+      query: {
+        mainQuery: false,
+        type: 'partOfSection',
       },
     },
   },
