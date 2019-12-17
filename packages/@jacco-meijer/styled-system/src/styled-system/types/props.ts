fe8eeb4c56721ac2f166d3a815316be606f1a824
Jacco Meijer <jacco@jaccomeijer.nl>
2019-12-17 15:08:50 +0100

    chore: moved styled-system to separate repo

--- packages/@jacco-meijer/styled-system/src/styled-system/types/props.ts
+++
@@ -1,5 +0,0 @@
-/** A simple string or number */
-export type StaticProp = string | number
-
-/** An array of StaticProps that represent a value for each breakpoint */
-export type ResponsiveProp = StaticProp[]
