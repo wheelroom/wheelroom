5a0969cd270ab3def67d817f8288542efee06a26
Jacco Meijer <jaccom@emerson-hotel.local>
2019-12-27 14:37:51 +0100

    chore: simplify jest config

--- packages/styled-system/jest.config.js
+++ packages/styled-system/jest.config.js
@@ -3,13 +3,4 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
   },
-
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/styled-system.ts',
-    '!**/node_modules/**',
-    '!<rootDir>/dist/**',
-    '!**/*.{int,unit}.test.{ts,tsx,js,jsx}',
-  ],
-  coverageReporters: ['lcov', 'text-summary'],
 }
