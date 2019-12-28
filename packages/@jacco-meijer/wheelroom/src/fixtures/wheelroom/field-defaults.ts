0c13c0d57c907a51c2ff5b408002f1a7987b92ae
Jacco Meijer <jaccom@emerson-hotel.local>
2019-12-28 22:33:31 +0100

    chore: sort fields

--- packages/@jacco-meijer/wheelroom/src/fixtures/wheelroom/field-defaults.ts
+++ packages/@jacco-meijer/wheelroom/src/fixtures/wheelroom/field-defaults.ts
@@ -7,9 +7,9 @@
 import { Field } from '../../types/fields'
 
 export const fieldDefaults: Field = {
-  type: 'shortText',
   helpText: '%Field name% for %component name%',
   initialContent: 'Demo content for %component name% %field name%',
   localized: false,
   required: false,
+  type: 'shortText',
 }
