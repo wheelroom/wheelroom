ac9da6c0071229156a0c50acb5e2926ca45200fc
Jacco Meijer <jacco@jaccomeijer.nl>
2020-01-08 17:18:51 +0100

    chore: add some demo markup

--- packages/boilerplate/src/components/opener-section/opener-section-home-opener-var.tsx
+++ packages/boilerplate/src/components/opener-section/opener-section-home-opener-var.tsx
@@ -7,14 +7,30 @@
  */
 
 import React from 'react'
-
+import { heading1Style } from '../../styles/heading'
+import { paragraphStyle } from '../../styles/paragraph'
+import { Box, Container, Flex } from '../../views/core-elements/grid'
+import { Heading } from '../../views/core-elements/heading'
+import { Text } from '../../views/core-elements/text'
+import { Image } from '../../views/image/image'
+import { Logo } from './logo'
 import { OpenerSectionProps } from './opener-section'
 
 export const OpenerSectionHomeOpenerVar = (props: OpenerSectionProps) => {
   return (
-    <div>
-      <h2>OpenerSectionHomeOpenerVar</h2>
-      <p>Hello world</p>
-    </div>
+    <Container>
+      <Flex>
+        <Box ncss={{ w: [1, 1 / 2], bg: 'blue', p: 10 }}>
+          <Heading ncss={heading1Style}>Large opener</Heading>
+          <Text ncss={paragraphStyle}>Doen logisch maken</Text>
+        </Box>
+        <Box ncss={{ w: [1, 1 / 2], bg: 'orange', p: 0 }}>
+          <Image image={props.image} />
+        </Box>
+      </Flex>
+      <Flex ncss={{ bg: 'orange', w: 1, p: 5 }}>
+        <Logo />
+      </Flex>
+    </Container>
   )
 }
