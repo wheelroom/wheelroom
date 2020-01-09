1c4bc80a24cdc43d519a9903ffd13e97bdbb2ea4
Jacco Meijer <jacco@jaccomeijer.nl>
2020-01-09 14:22:33 +0100

    chore: better message

--- packages/@jacco-meijer/wheelroom-plugin-contentful/src/lib/components-found.ts
+++ packages/@jacco-meijer/wheelroom-plugin-contentful/src/lib/components-found.ts
@@ -2,7 +2,7 @@ import { ContentfulComponents } from '../types/contentful-components'
 
 export const componentsFound = (components: ContentfulComponents) => {
   if (Object.keys(components).length === 0) {
-    console.log('Aborting because there are no components to process')
+    console.log('Aborting: no components to process')
     return false
   }
   return true
