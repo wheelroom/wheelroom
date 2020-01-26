173ee139d4a342fe15b4ee1ab6eab22b5b38904c
Jacco Meijer <jaccom@emerson-hotel.local>
2020-01-27 00:33:56 +0100

    feat: remove %tsModels% and %tsModelTypes%

--- packages/@jacco-meijer/wheelroom-plugin-boilerplate/src/lib/parser/replace-functions/index.ts
+++ packages/@jacco-meijer/wheelroom-plugin-boilerplate/src/lib/parser/replace-functions/index.ts
@@ -1,7 +1,5 @@
 import { componentHtmlAttributesFunc } from './component-html-attributes/component-html-attributes'
 import { reactPropsFunc } from './react-props/react-props'
-import { tsModelTypesFunc } from './ts-model-types/ts-model-types'
-import { tsModelsFunc } from './ts-models/ts-models'
 import { variationImportListFunc } from './variation-import-list/variation-import-list'
 import { variationListFunc } from './variation-list/variation-list'
 import { variationFunc } from './variation/variation'
@@ -9,8 +7,6 @@ import { variationFunc } from './variation/variation'
 export const replaceFunctions = [
   ...componentHtmlAttributesFunc,
   ...reactPropsFunc,
-  ...tsModelsFunc,
-  ...tsModelTypesFunc,
   ...variationFunc,
   ...variationImportListFunc,
   ...variationListFunc,
