65fa8a6868c049cfd7cdd221131afc1abe1d2910
Jacco Meijer <jacco@jaccomeijer.nl>
2020-02-12 15:43:29 +0100

    chore: update content types

--- packages/boilerplate/src/config/plugin-contentful/content-types.ts
+++ packages/boilerplate/src/config/plugin-contentful/content-types.ts
@@ -1,98 +1,86 @@
 interface FooterSectionModel {
   fields: {
     backgroundColor: 'green' | 'mint'
-    footerNavigation: 'openerNavigatie' | 'footerNavigatie'
+    navigation: 'openerNavigatie' | 'footerNavigatie'
     title: string
   }
   model: 'footerSection'
 }
 interface GlobalsModel {
   fields: {
-    addressLine1: string
-    addressLine2: string
-    emailAddress: string
+    addressLine1?: string
+    addressLine2?: string
+    emailAddress?: string
     linkedinUrl?: string
     phoneNumber?: string
     siteAuthor?: string
     siteDescription?: string
-    siteHeading: string
+    siteHeading?: string
     siteKeywords?: string[]
     title: string
   }
   model: 'globals'
 }
+interface ListItemModel {
+  fields: {
+    view?: 'onderkant mint' | 'onderkant groen' | 'volledige afbeelding'
+    link?: 'homePage' | 'productPage' | 'backgroundPage' | 'contactPage'
+    heading?: string
+    image?: boolean
+    abstract?: string
+    extraText?: string
+    title: string
+  }
+  model: 'listItem'
+}
 interface ListSectionModel {
   fields: {
-    heading: string
-    listItems: Array<
-      'homePage' | 'productPage' | 'backgroundPage' | 'contactPage'
-    >
-    variation: 'large items' | 'small items'
+    variation: 'grote tegels' | 'kleine tegels'
+    items: string[]
+    heading?: string
     title: string
   }
   model: 'listSection'
 }
 interface NavigationModel {
   fields: {
-    routes: Array<'homePage' | 'productPage' | 'backgroundPage' | 'contactPage'>
+    pages: Array<'homePage' | 'productPage' | 'backgroundPage' | 'contactPage'>
     title: string
   }
   model: 'navigation'
 }
 interface OpenerSectionModel {
   fields: {
-    boxBackgroundColor?: 'green' | 'mint'
+    variation: 'home pagina' | 'normale pagina'
+    navigation: 'openerNavigatie' | 'footerNavigatie'
+    boxBackgroundColor: 'green' | 'mint'
     heading?: string
     image?: boolean
-    infoText?: string
-    mainNavigation: 'openerNavigatie' | 'footerNavigatie'
-    variation: 'home opener' | 'page opener'
+    abstract?: string
     title: string
   }
   model: 'openerSection'
 }
 interface PageModel {
   fields: {
-    navigationHeading?: string
-    pageHeading: string
-    pageImage: boolean
-    pageInfoText: string
     path: string
-    pathName: string
-    sections: Array<
-      | 'homeOpener'
-      | 'productOpener'
-      | 'backgroundOpener'
-      | 'contactOpener'
-      | 'contactText'
-      | 'productcodeText'
-      | 'merkverwateringText'
-      | 'merkarchitectuurText'
-      | 'naamcreatieText'
-      | 'careQuote'
-      | 'buildLoveQuote'
-      | 'passionQuote'
-      | 'endOfPageLinks'
-      | 'endOfPageLinksLarge'
-      | 'standardFooter'
-    >
-    seoDescription: string
-    seoTitle: string
-    listItemHeading?: string
-    listItemHiddenText?: string
-    listItemImage?: boolean
-    listItemInfoText?: string
-    listItemView?: 'mint bottom' | 'green bottom' | 'full image'
+    sections: Array<'homeOpener' | 'productOpener' | 'backgroundOpener' | 'contactOpener' | 'contactText' | 'productcodeText' | 'merkverwateringText' | 'merkarchitectuurText' | 'naamcreatieText' | 'careQuote' | 'buildLoveQuote' | 'passionQuote' | 'endOfPageLinks' | 'endOfPageLinksLarge' | 'standardFooter'>
+    heading: string
+    navigationHeading?: string
+    image?: boolean
+    abstract?: string
+    seoTitle?: string
+    seoDescription?: string
     title: string
   }
   model: 'page'
 }
 interface QuoteSectionModel {
   fields: {
-    avatar?: boolean
     heading: string
-    subHeading?: string
-    text: string
+    subHeading: string
+    abstract: string
+    image?: boolean
     title: string
   }
   model: 'quoteSection'
@@ -107,6 +95,7 @@ interface TextSectionModel {
 type Model =
   | FooterSectionModel
   | GlobalsModel
+  | ListItemModel
   | ListSectionModel
   | NavigationModel
   | OpenerSectionModel
