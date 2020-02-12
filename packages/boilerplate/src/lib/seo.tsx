e25bd7215526053efa6061ed666e212fea78b945
Jacco Meijer <jacco@jaccomeijer.nl>
2020-02-12 16:56:50 +0100

    remove unused getSeoContentTypeInfo

--- packages/boilerplate/src/lib/seo.tsx
+++ packages/boilerplate/src/lib/seo.tsx
@@ -138,32 +138,3 @@ export const Seo = (context: SeoProps) => {
     </Helmet>
   )
 }
-
-export const getSeoContentTypeInfo = (
-  page: any,
-  article: any
-): SeoContentTypeInfo => {
-  const noInfo = {
-    type: 'none',
-  } as SeoContentTypeInfo
-  if (!article) {
-    return noInfo
-  }
-
-  // Do we have an article?
-  if (page.pathName === 'article') {
-    return {
-      tags: {
-        author: article.author,
-        modified_time: article.updatedAt,
-        published_time: article.createdAt,
-        section: 'Technology',
-        // expiration_time: '',
-        // tag: '',
-      },
-      type: 'article',
-    } as SeoContentTypeInfo
-  }
-
-  return noInfo
-}
