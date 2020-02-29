b5d0ec529dbb798acca12b4c128b310dd8e26a54
Jacco Meijer <jacco@jaccomeijer.nl>
2020-02-29 22:05:15 +0100

    chore: add headings

--- packages/wheelroom-io/src/views/core-elements/heading.tsx
+++ packages/wheelroom-io/src/views/core-elements/heading.tsx
@@ -5,80 +5,43 @@ import {
   heading1Style,
   heading2Style,
   heading3Style,
+  heading4Style,
+  heading5Style,
+  heading6Style,
 } from '../../styles/heading'
 
-/**
- *
- * Optie A - Bestaand
- *
- * Heeft als nadeel dat je altijd zelf de heading stijl moet toevoegen. Dat gaat
- * prima, maar is in de praktijk wat omslachtig.
- *
- */
-
 export const H1 = (props: any) => (
-  <h1 css={emotionCss({ ncss: props.ncss })}>{props.children}</h1>
-)
-
-export const H2 = (props: any) => (
-  <h2 css={emotionCss({ ncss: props.ncss })}>{props.children}</h2>
-)
-
-export const H3 = (props: any) => (
-  <h3 css={emotionCss({ ncss: props.ncss })}>{props.children}</h3>
-)
-
-export const H4 = (props: any) => (
-  <h4 css={emotionCss({ ncss: props.ncss })}>{props.children}</h4>
-)
-
-export const H5 = (props: any) => (
-  <h5 css={emotionCss({ ncss: props.ncss })}>{props.children}</h5>
-)
-
-export const H6 = (props: any) => (
-  <h6 css={emotionCss({ ncss: props.ncss })}>{props.children}</h6>
-)
-
-/**
- *
- * Optie B - Met createElement en is-element prop
- *
- * Heeft als nadeel dat je, net als bij de huidige implementatie, altijd zelf de
- * stijl moet toevoegen
- *
- */
-export const Heading = (props: { ncss: any; children: any; is: string }) => {
-  const style = { css: emotionCss({ ncss: props.ncss || {} }) }
-  return jsx(props.is || 'h1', style, props.children)
-}
-
-export const H1B = (props: any) => <Heading {...props} is="h1" />
-export const H2B = (props: any) => <Heading {...props} is="h2" />
-export const H3B = (props: any) => <Heading {...props} is="h3" />
-
-/**
- *
- * Optie C - Individueel en meteen stijl toevoegen
- *
- * Misschien wat verbose, maar wel de meest prettige oplossing omdat meteen de
- * juiste stijl kan worden meegegeven.
- *
- */
-export const H1C = (props: any) => (
   <h1 css={emotionCss({ ncss: { ...heading1Style, ...props.ncss } })}>
     {props.children}
   </h1>
 )
 
-export const H2C = (props: any) => (
+export const H2 = (props: any) => (
   <h2 css={emotionCss({ ncss: { ...heading2Style, ...props.ncss } })}>
     {props.children}
   </h2>
 )
 
-export const H3C = (props: any) => (
+export const H3 = (props: any) => (
   <h3 css={emotionCss({ ncss: { ...heading3Style, ...props.ncss } })}>
     {props.children}
   </h3>
+)
+
+export const H4 = (props: any) => (
+  <h4 css={emotionCss({ ncss: { ...heading4Style, ...props.ncss } })}>
+    {props.children}
+  </h4>
+)
+
+export const H5 = (props: any) => (
+  <h5 css={emotionCss({ ncss: { ...heading5Style, ...props.ncss } })}>
+    {props.children}
+  </h5>
+)
+
+export const H6 = (props: any) => (
+  <h6 css={emotionCss({ ncss: { ...heading6Style, ...props.ncss } })}>
+    {props.children}
+  </h6>
 )
