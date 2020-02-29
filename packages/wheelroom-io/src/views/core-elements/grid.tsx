b8811648ee7530ff3f797fa51d1a45520c63df9c
Jacco Meijer <jacco@jaccomeijer.nl>
2020-02-29 22:29:26 +0100

    feat: add is prop to grid

--- packages/wheelroom-io/src/views/core-elements/grid.tsx
+++ packages/wheelroom-io/src/views/core-elements/grid.tsx
@@ -2,45 +2,81 @@
 import { jsx } from '@emotion/core'
 import { emotionCss } from './emotion-css'
 
-export const Box = (props: any) => (
-  <div
-    onClick={props.onClick}
-    css={emotionCss({
-      ncss: { boxSizing: 'border-box', ...props.ncss },
-    })}
-  >
-    {props.children}
-  </div>
-)
+export type ElementName =
+  | 'a'
+  | 'applet'
+  | 'button'
+  | 'form'
+  | 'frame'
+  | 'iframe'
+  | 'img'
+  | 'input'
+  | 'map'
+  | 'meta'
+  | 'object'
+  | 'param'
+  | 'select'
+  | 'textarea>'
 
-export const Flex = (props: any) => (
-  <div
-    onClick={props.onClick}
-    css={emotionCss({
-      ncss: {
-        boxSizing: 'border-box',
-        display: 'flex',
-        flexWrap: 'wrap',
-        ...props.ncss,
-      },
-    })}
-  >
-    {props.children}
-  </div>
-)
+export interface GridProps {
+  /** Render as another HTML element */
+  is: ElementName
+  /** React children */
+  children: any
+  /** Nested emotion css styling */
+  ncss: any
+}
 
-export const Container = (props: any) => {
-  return (
-    <div
-      css={emotionCss({
-        ncss: {
-          mx: 'auto',
-          maxWidth: '1200px',
-          ...props.ncss,
-        },
-      })}
-    >
-      {props.children}
-    </div>
-  )
+export interface BoxProps extends GridProps {
+  onClick: () => any
+}
+
+export const Box = (props: BoxProps) => {
+  const css = emotionCss({
+    ncss: { boxSizing: 'border-box', ...props.ncss },
+  })
+  const attrs = {
+    css,
+    onClick: props.onClick,
+  }
+  return jsx(props.is || 'div', attrs, props.children)
+}
+
+export interface FlexProps extends GridProps {
+  onClick: () => any
+}
+
+export const Flex = (props: FlexProps) => {
+  const css = emotionCss({
+    ncss: {
+      boxSizing: 'border-box',
+      display: 'flex',
+      flexWrap: 'wrap',
+      ...props.ncss,
+    },
+  })
+  const attrs = {
+    css,
+    onClick: props.onClick,
+  }
+  return jsx(props.is || 'div', attrs, props.children)
+}
+
+export interface ContainerProps extends GridProps {
+  onClick: () => any
+}
+
+export const Container = (props: ContainerProps) => {
+  const css = emotionCss({
+    ncss: {
+      mx: 'auto',
+      maxWidth: '1200px',
+      ...props.ncss,
+    },
+  })
+  const attrs = {
+    css,
+    onClick: props.onClick,
+  }
+  return jsx(props.is || 'div', attrs, props.children)
 }
