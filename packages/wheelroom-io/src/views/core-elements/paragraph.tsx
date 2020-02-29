d9eca976b792b38b379ce6e2c0828b96e3d1c24c
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-02-29 23:48:43 +0100

    chore: reverse implementation

--- packages/wheelroom-io/src/views/core-elements/paragraph.tsx
+++ packages/wheelroom-io/src/views/core-elements/paragraph.tsx
@@ -2,18 +2,13 @@
 import { jsx } from '@emotion/core'
 import { emotionCss } from './emotion-css'
 
-export const Paragraph = (props: any) => (
+export interface ParagraphProps {
+  /** React children */
+  children?: any
+  /** Nested emotion css styling */
+  ncss?: any
+}
+
+export const Paragraph = (props: ParagraphProps) => (
   <p css={emotionCss({ ncss: props.ncss })}>{props.children}</p>
-)
-
-export const Span = (props: any) => (
-  <span css={emotionCss({ ncss: props.ncss })}>{props.children}</span>
-)
-
-export const Ul = (props: any) => (
-  <ul css={emotionCss({ ncss: props.ncss })}>{props.children}</ul>
-)
-
-export const Li = (props: any) => (
-  <li css={emotionCss({ ncss: props.ncss })}>{props.children}</li>
 )
