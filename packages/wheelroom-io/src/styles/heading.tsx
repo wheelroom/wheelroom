4daf82f9253c23113daac30e3db867dc7cbd204e
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-02-29 15:13:28 +0100

    Edit heading styling

--- packages/wheelroom-io/src/styles/heading.tsx
+++ packages/wheelroom-io/src/styles/heading.tsx
@@ -2,58 +2,58 @@ export const heading1Style = {
   color: 'black',
   fontFamily: 'heading',
   fontSize: [6, 7, 8, 9],
-  fontWeight: 7,
-  lineHeight: [3, 3, 4, 4],
-  pb: [2, 2, 3, 4],
+  mt: 0,
+  mb: 3,
 }
 
 export const heading2Style = {
   color: 'black',
   fontFamily: 'heading',
   fontSize: [5, 6, 7, 8],
-  fontWeight: 7,
-  lineHeight: [3, 3, 4, 4],
-  pb: [1, 2, 3, 4],
+  mt: 0,
+  mb: 3,
 }
 
 export const heading3Style = {
   color: 'black',
   fontFamily: 'heading',
   fontSize: [4, 5, 6, 7],
-  fontWeight: 7,
-  lineHeight: [3, 3, 4, 4],
-  pb: [1, 1, 2, 2],
+  mt: 0,
+  mb: 3,
 }
 
 export const heading4Style = {
   color: 'black',
   fontFamily: 'heading',
   fontSize: [3, 4, 5, 6],
-  fontWeight: 7,
-  lineHeight: [3, 3, 4, 4],
+  mt: 0,
+  mb: 3,
 }
 
 export const heading5Style = {
   color: 'black',
   fontFamily: 'heading',
   fontSize: [3, 4, 5, 6],
-  fontWeight: 7,
-  lineHeight: [3, 3, 4, 4],
+  mt: 0,
+  mb: 3,
 }
 
 export const heading6Style = {
   color: 'black',
   fontFamily: 'heading',
   fontSize: [3, 4, 5, 6],
-  fontWeight: 7,
-  lineHeight: [3, 3, 4, 4],
+  mt: 0,
+  mb: 3,
 }
 
 export const navigationHeadingStyle = {
-  color: 'black',
-  fontFamily: 'heading',
+  color: 'azure',
+  fontFamily: 'text',
   fontSize: [3, 4],
-  fontWeight: 4,
-  lineHeight: [3, 3, 4, 4],
-  pb: [1, 2, 3, 4],
+  m: 0,
+  mr: 3,
+  textDecoration: 'none',
+  '&:hover': { textDecoration: 'underline' },
+  '&:focus': { textDecoration: 'underline', color: 'amber' },
+  '&:last-child': { mr: 0 },
 }
