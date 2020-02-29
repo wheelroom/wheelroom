76d3602b933b6ce814993f61326a60020a21386f
Jacco Meijer <jacco@jaccomeijer.nl>
2020-02-29 22:13:32 +0100

    chore: add types

--- packages/wheelroom-io/src/views/core-elements/heading.tsx
+++ packages/wheelroom-io/src/views/core-elements/heading.tsx
@@ -10,37 +10,42 @@ import {
   heading6Style,
 } from '../../styles/heading'
 
-export const H1 = (props: any) => (
+interface HeadingProps {
+  ncss: any
+  children: any
+}
+
+export const H1 = (props: HeadingProps) => (
   <h1 css={emotionCss({ ncss: { ...heading1Style, ...props.ncss } })}>
     {props.children}
   </h1>
 )
 
-export const H2 = (props: any) => (
+export const H2 = (props: HeadingProps) => (
   <h2 css={emotionCss({ ncss: { ...heading2Style, ...props.ncss } })}>
     {props.children}
   </h2>
 )
 
-export const H3 = (props: any) => (
+export const H3 = (props: HeadingProps) => (
   <h3 css={emotionCss({ ncss: { ...heading3Style, ...props.ncss } })}>
     {props.children}
   </h3>
 )
 
-export const H4 = (props: any) => (
+export const H4 = (props: HeadingProps) => (
   <h4 css={emotionCss({ ncss: { ...heading4Style, ...props.ncss } })}>
     {props.children}
   </h4>
 )
 
-export const H5 = (props: any) => (
+export const H5 = (props: HeadingProps) => (
   <h5 css={emotionCss({ ncss: { ...heading5Style, ...props.ncss } })}>
     {props.children}
   </h5>
 )
 
-export const H6 = (props: any) => (
+export const H6 = (props: HeadingProps) => (
   <h6 css={emotionCss({ ncss: { ...heading6Style, ...props.ncss } })}>
     {props.children}
   </h6>
