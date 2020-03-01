5fcf5ab0372021afa720110ba8be45b372cf8506
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-01 19:22:24 +0100

    feat: make heading ncss optional prop

--- packages/wheelroom-io/src/views/core-elements/heading.tsx
+++ packages/wheelroom-io/src/views/core-elements/heading.tsx
@@ -11,42 +11,55 @@ import {
 } from '../../styles/heading'
 
 interface HeadingProps {
-  ncss: any
+  ncss?: any
   children: any
 }
 
-export const H1 = (props: HeadingProps) => (
-  <h1 css={systemCss({ ncss: { ...heading1Style, ...props.ncss } })}>
-    {props.children}
-  </h1>
-)
-
-export const H2 = (props: HeadingProps) => (
-  <h2 css={systemCss({ ncss: { ...heading2Style, ...props.ncss } })}>
-    {props.children}
-  </h2>
-)
-
-export const H3 = (props: HeadingProps) => (
-  <h3 css={systemCss({ ncss: { ...heading3Style, ...props.ncss } })}>
-    {props.children}
-  </h3>
-)
-
-export const H4 = (props: HeadingProps) => (
-  <h4 css={systemCss({ ncss: { ...heading4Style, ...props.ncss } })}>
-    {props.children}
-  </h4>
-)
-
-export const H5 = (props: HeadingProps) => (
-  <h5 css={systemCss({ ncss: { ...heading5Style, ...props.ncss } })}>
-    {props.children}
-  </h5>
-)
-
-export const H6 = (props: HeadingProps) => (
-  <h6 css={systemCss({ ncss: { ...heading6Style, ...props.ncss } })}>
-    {props.children}
-  </h6>
-)
+export const H1 = (props: HeadingProps) => {
+  const ncss = props.ncss || {}
+  return (
+    <h1 css={systemCss({ ncss: { ...heading1Style, ...ncss } })}>
+      {props.children}
+    </h1>
+  )
+}
+export const H2 = (props: HeadingProps) => {
+  const ncss = props.ncss || {}
+  return (
+    <h1 css={systemCss({ ncss: { ...heading2Style, ...ncss } })}>
+      {props.children}
+    </h1>
+  )
+}
+export const H3 = (props: HeadingProps) => {
+  const ncss = props.ncss || {}
+  return (
+    <h1 css={systemCss({ ncss: { ...heading3Style, ...ncss } })}>
+      {props.children}
+    </h1>
+  )
+}
+export const H4 = (props: HeadingProps) => {
+  const ncss = props.ncss || {}
+  return (
+    <h1 css={systemCss({ ncss: { ...heading4Style, ...ncss } })}>
+      {props.children}
+    </h1>
+  )
+}
+export const H5 = (props: HeadingProps) => {
+  const ncss = props.ncss || {}
+  return (
+    <h1 css={systemCss({ ncss: { ...heading5Style, ...ncss } })}>
+      {props.children}
+    </h1>
+  )
+}
+export const H6 = (props: HeadingProps) => {
+  const ncss = props.ncss || {}
+  return (
+    <h1 css={systemCss({ ncss: { ...heading6Style, ...ncss } })}>
+      {props.children}
+    </h1>
+  )
+}
