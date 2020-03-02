f6ec3ddc45f6837c781ce34b52fa1c35a106a6ab
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-03-02 12:59:40 +0100

    Update Navigation structure

--- packages/wheelroom-io/src/components/navigation/navigation-single-var.tsx
+++ packages/wheelroom-io/src/components/navigation/navigation-single-var.tsx
@@ -10,25 +10,31 @@ import React from 'react'
 import { NavLinks } from './nav-links'
 import { NavigationProps } from './navigation'
 import { List } from '../../views/core-elements/list'
-import { Any } from '../../views/core-elements/any'
+import { GLink } from '../../views/core-elements/g-link'
+import { Flex } from '../../views/core-elements/grid'
 
 export const NavigationSingleVar = (props: NavigationProps) => {
   return (
-    <Any is={'nav'} ncss={{ label: 'Nav' }}>
-      <List
-        ncss={{
-          label: 'NavItems',
-          display: 'flex',
-          flexDirection: 'row',
-          listStyle: 'none',
-          flexWrap: 'wrap',
-          mb: 0,
-          mt: 0,
-          pl: 0,
-        }}
-      >
-        <NavLinks pages={props.pages} />
-      </List>
-    </Any>
+    <Flex>
+      <GLink to="/" aria-label="Wheelroom, Back to homepage">
+        Wheelroom Logo
+      </GLink>
+      <Flex is={'nav'} ncss={{ label: 'Nav' }}>
+        <List
+          ncss={{
+            label: 'NavItems',
+            display: 'flex',
+            flexDirection: 'row',
+            listStyle: 'none',
+            flexWrap: 'wrap',
+            mb: 0,
+            mt: 0,
+            pl: 0,
+          }}
+        >
+          <NavLinks pages={props.pages} />
+        </List>
+      </Flex>
+    </Flex>
   )
 }
