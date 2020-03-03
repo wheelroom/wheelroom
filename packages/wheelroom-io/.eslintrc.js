4f5a245b304033f281e656f3d70ede3dc838a91f
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-03-03 01:43:15 +0100

    Merge branch 'master' of github.com:wheelroom/wheelroom-io

--- packages/wheelroom-io/.eslintrc.js
+++ packages/wheelroom-io/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
+  plugins: ['react-hooks'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
@@ -18,6 +19,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
     '@typescript-eslint/camelcase': 0,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
   },
   settings: {
     react: {
