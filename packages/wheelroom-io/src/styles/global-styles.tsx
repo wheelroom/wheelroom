dc7b2abfd6da949d8551ebb1bc894600b2588ee5
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-03-05 17:40:52 +0100

    chore: convert CSS rules to JavaScript style objects

--- packages/wheelroom-io/src/styles/global-styles.tsx
+++ packages/wheelroom-io/src/styles/global-styles.tsx
@@ -1,15 +1,10 @@
-export const GlobalStyles = `
-html {
-  box-sizing: border-box;
+export const GlobalStyles = {
+  html: { boxSizing: 'border-box' },
+  '*': { boxSizing: 'inherit', backfaceVisibility: 'hidden' },
+  ':before': { boxSizing: 'inherit' },
+  ':after': { boxSizing: 'inherit' },
+  ':focus': {
+    outline: '2px dotted #FCA623',
+    outlineOffset: '5px',
+  },
 }
-* {
-  backface-visibility: hidden;
-}
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-:focus {
-  outline: 2px dotted #FCA623;
-  outline-offset: 5px;
-}
-`
