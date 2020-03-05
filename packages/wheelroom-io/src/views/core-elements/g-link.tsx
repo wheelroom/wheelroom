fd151d920502aa5b71559dc7b5f1f6b3efa85760
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-03-05 23:44:41 +0100

    fix: export GLink props

--- packages/wheelroom-io/src/views/core-elements/g-link.tsx
+++ packages/wheelroom-io/src/views/core-elements/g-link.tsx
@@ -7,7 +7,16 @@ import { AdminCoreContext } from '@jacco-meijer/admin-core'
 import { getPreviewQueryString } from '@jacco-meijer/admin-page-preview'
 import { useGetCurrentThemeId } from '@jacco-meijer/admin-theme-switcher'
 
-export const GLink = (props: any) => {
+export interface GLinkProps {
+  /** React children */
+  children?: any
+  /** Nested emotion css styling */
+  ncss?: any
+  /** Gatsby Link */
+  to?: any
+}
+
+export const GLink = (props: GLinkProps) => {
   const currentThemeId = useGetCurrentThemeId() as ThemeId
   const { adminCoreState } = useContext(AdminCoreContext)
   if (!props.to) {
