0222b08b1ae424f3ed3250f2a82e8c85cd7894fb
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-03-06 13:47:39 +0100

    chore: Add ALinkProps to Alink component

--- packages/wheelroom-io/src/views/core-elements/a-link.tsx
+++ packages/wheelroom-io/src/views/core-elements/a-link.tsx
@@ -3,7 +3,16 @@ import { jsx } from '@emotion/core'
 import { systemCss, ThemeId } from '../../styled-system/system-css'
 import { useGetCurrentThemeId } from '@jacco-meijer/admin-theme-switcher'
 
-export const ALink = (props: any) => {
+export interface ALinkProps {
+  /** React children */
+  children?: any
+  /** Nested emotion css styling */
+  ncss?: any
+  /** Gatsby Link */
+  href?: any
+}
+
+export const ALink = (props: ALinkProps) => {
   const currentThemeId = useGetCurrentThemeId() as ThemeId
 
   return (
