efc7c8cd65de17a8a66493277d3aafea66853525
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-03-06 13:56:47 +0100

    chore: footer section proposal

--- packages/wheelroom-io/src/components/footer-section/footer-section-single-var.tsx
+++ packages/wheelroom-io/src/components/footer-section/footer-section-single-var.tsx
@@ -8,11 +8,13 @@
 
 import React from 'react'
 import { paragraph1Style } from '../../styles/paragraph'
+import { simpleLinkStyle } from '../../styles/simple-link'
 import { Box, Container, Flex } from '../../views/core-elements/grid'
 import { Paragraph } from '../../views/core-elements/paragraph'
 import { List } from '../../views/core-elements/list'
 import { NavLinks } from '../navigation/nav-links'
 import { FooterSectionProps } from './footer-section'
+import { ALink } from '../../views/core-elements/a-link'
 
 const wrapperStyle = {
   label: 'Wrapper',
@@ -24,8 +26,31 @@ const wrapperStyle = {
 
 const containerStyle = {
   label: 'Container',
+  fontFamily: 'text',
+  color: 'text',
   height: '100%',
+  maxWidth: '1280px',
+  py: 4,
   justifyContent: 'space-between',
+}
+
+const legalStyle = {
+  label: 'Legal',
+  display: 'flex',
+  flex: '1',
+}
+
+const listStyle = {
+  label: 'NavItems',
+  display: 'flex',
+  justifyContent: 'flex-end',
+  flex: '1',
+  flexDirection: 'row',
+  listStyle: 'none',
+  flexWrap: 'wrap',
+  mb: 0,
+  mt: 0,
+  pl: 0,
 }
 
 const addressStyle = { ...paragraph1Style }
@@ -34,11 +59,6 @@ export const FooterSectionSingleVar = (props: FooterSectionProps) => {
   return (
     <Box is="div" ncss={wrapperStyle}>
       <Container ncss={containerStyle}>
-        <Flex is="div">
-          <List is="ul">
-            <NavLinks pages={props.navigation.pages} />
-          </List>
-        </Flex>
         <Flex is="div">
           <Box>
             <Paragraph ncss={addressStyle}>
@@ -51,6 +71,34 @@ export const FooterSectionSingleVar = (props: FooterSectionProps) => {
               {props.globals.emailAddress}
             </Paragraph>
           </Box>
+        </Flex>
+        <Flex is="div">
+          <List is="ul">
+            <NavLinks pages={props.navigation.pages} />
+          </List>
+        </Flex>
+      </Container>
+      <Container ncss={containerStyle}>
+        <Flex>
+          Select Language
+          <ALink ncss={{ ...simpleLinkStyle, mx: 2 }} href="/en">
+            English
+          </ALink>
+        </Flex>
+        <Flex is="div" ncss={legalStyle}>
+          <List is="ul" ncss={listStyle}>
+            <List is="li">
+              <ALink ncss={{ ...simpleLinkStyle, mr: 4 }} href="/privacy">
+                Privacy & Cookies
+              </ALink>
+            </List>
+            <List is="li">
+              <ALink ncss={{ ...simpleLinkStyle, mr: 4 }} href="/sitemap">
+                Sitemap
+              </ALink>
+            </List>
+          </List>
+          <Box ncss={{ color: 'metal' }}><small>Wheelroom v1.0.0</small></Box>
         </Flex>
       </Container>
     </Box>
