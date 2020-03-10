f8d2e1d15a78545a2519ea56cc373627fcb8158b
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-10 13:00:27 +0100

    fix: actually export new command

--- packages/@jacco-meijer/wheelroom-plugin-contentful/src/commands/commands.ts
+++ packages/@jacco-meijer/wheelroom-plugin-contentful/src/commands/commands.ts
@@ -1,11 +1,13 @@
 import { command as createContentCmd } from './create-content/command'
 import { command as createModelsCmd } from './create-models/command'
 import { command as deleteContentCmd } from './delete-content/command'
+import { command as deleteModelsCmd } from './delete-models/command'
 import { command as listModlesCmd } from './list-models/command'
 
 export const commands = [
   createContentCmd,
   createModelsCmd,
   deleteContentCmd,
+  deleteModelsCmd,
   listModlesCmd,
 ]
