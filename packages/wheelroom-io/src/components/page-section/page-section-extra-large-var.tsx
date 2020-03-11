f7958b49279e9e6f32a8b3e6366279c111b1965b
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-11 12:25:22 +0100

    feat: add bare menu

--- packages/wheelroom-io/src/components/page-section/page-section-extra-large-var.tsx
+++ packages/wheelroom-io/src/components/page-section/page-section-extra-large-var.tsx
@@ -11,11 +11,16 @@ import { PageSectionProps } from './page-section'
 import { getPageSectionInfo } from '../../lib/get-page-section-info'
 import { Hero } from './extra-large/hero'
 import { NotImplemented } from '../../lib/not-implemented'
+import { Navigation } from '../navigation'
 
 export const PageSectionExtraLargeVar = (props: PageSectionProps) => {
   const info = getPageSectionInfo(props)
-  if (info.hasNavigation && info.hasTopic) {
-    return <Hero topic={props.topics[0]} navigation={props.navigation}></Hero>
+  if (info.hasNavigation && info.hasTopic && !info.hasText) {
+    return <Hero topic={props.topics[0]} navigation={props.navigation} />
   }
+  if (info.hasNavigation && !info.hasTopic && !info.hasText) {
+    return <Navigation actions={props.navigation.actions} />
+  }
+
   return <NotImplemented {...props} />
 }
