34323b90059bfcba5bc6d86c37ed9abda560aab1
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-11 12:18:34 +0100

    fix: update component template

--- packages/wheelroom-io/src/config/plugin-boilerplate/component-variation.ts
+++ packages/wheelroom-io/src/config/plugin-boilerplate/component-variation.ts
@@ -6,52 +6,11 @@ export const componentVariation = `/**
  *
  */
 
-import React, { Fragment } from 'react'
-import { Box, Flex } from '../../views/core-elements/grid'
-import { Image } from '../../views/image/image'
+import React from 'react'
 import { %ComponentName%Props } from './%component-name%'
-
-const FlexContainer = Flex
-const FlexBox = Box
-
-const flexContainerProps = {
-  ncss: {
-    bg: '#EEE',
-    fontFamily: 'text',
-    maxHeight: '300px',
-    mb: 1,
-    overflow: 'hidden',
-    p: 3,
-    w: [1],
-  },
-}
-
-const flexBoxProps = {
-  ncss: { w: [1, 1 / 4] },
-}
-
-const imageProps = {
-  objectFit: 'cover',
-}
+import { NotImplemented } from '../../lib/not-implemented'
 
 export const %ComponentName%%VariationName%Var = (props: %ComponentName%Props) => {
-  return (
-    <Fragment>
-      <FlexContainer
-        ncss={{
-          ...flexContainerProps.ncss,
-          bg: '#CCC',
-          fontFamily: 'heading',
-          fontSize: 7,
-          my: 4,
-        }}
-      >
-        <FlexBox ncss={{ ...flexBoxProps.ncss, w: 1 }}>
-          %Component name% %variation name% var
-        </FlexBox>
-      </FlexContainer>
-      %flexBoxProps(indent:6)%
-    </Fragment>
-  )
+  return <NotImplemented {...props} />
 }
 `
