4c1385e18149ce3383fb29cbfc78aa1c87b03600
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-11 11:11:08 +0100

    fix: remove console.log

--- packages/wheelroom-io/src/components/page-section/page-section-extra-large-var.tsx
+++ packages/wheelroom-io/src/components/page-section/page-section-extra-large-var.tsx
@@ -12,7 +12,6 @@ import { getPageSectionInfo } from '../../lib/get-page-section-info'
 import { Hero } from './extra-large/hero'
 
 export const PageSectionExtraLargeVar = (props: PageSectionProps) => {
-  console.log(props)
   const info = getPageSectionInfo(props)
   if (info.hasNavigation && info.hasTopic) {
     return <Hero topic={props.topics[0]} navigation={props.navigation}></Hero>
