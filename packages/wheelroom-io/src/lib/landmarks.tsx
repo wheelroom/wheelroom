0d97ce7b810c44e5fd721b457756d516aa4db62f
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-12 23:19:13 +0100

    feat: add landmarks

--- packages/wheelroom-io/src/lib/landmarks.tsx
+++ packages/wheelroom-io/src/lib/landmarks.tsx
@@ -1,14 +1,46 @@
 import { Fragment } from 'react'
 import React from 'react'
+import { PageSectionProps } from '../components/page-section/page-section'
+import { getPageSectionInfo } from './get-page-section-info'
 import { systemCss } from '../styled-system/system-css'
 import { jsx } from '@emotion/core'
 
-// This element wraps the landmark element specified in sections.jsx around each
-// section. Not that the landmarks are added in a fixed order as shown in the
-// Fragment below. As a consequence the order of the sections is bound to the
-// order of the landmarks below.
-//
+const getLandmark = (
+  pageProps: PageSectionProps,
+  sectionCount: number
+): string => {
+  const info = getPageSectionInfo(pageProps)
+  if (info.index === 0 && (info.variation === 'hero' || info.hasNavigation)) {
+    return 'header'
+  }
+  if (info.index === sectionCount - 1 && info.hasNavigation) {
+    return 'footer'
+  }
 
+  return 'main'
+}
+
+export const Landmark = (props: { variation: string; children: any }) => {
+  const label = 'variation-' + props.variation || 'single'
+  const css = systemCss({
+    ncss: { label },
+  })
+  const attrs = {
+    css,
+  }
+  return jsx('section', attrs, props.children)
+}
+
+/**
+ *
+ * This element wraps the landmark element specified in sections.jsx around each
+ * section. Not that the landmarks are added in a fixed order as shown in the
+ * Fragment below. As a consequence the order of the sections is bound to the
+ * order of the landmarks below.
+ *
+ * To change this behaviour, change the getLandmark method above.
+ *
+ */
 export const Landmarks = (props: any) => {
   const landMarkedChildren = {
     aside: [],
@@ -18,7 +50,9 @@ export const Landmarks = (props: any) => {
   } as any
 
   React.Children.forEach(props.children, child => {
-    landMarkedChildren[child.props.landMark].push(child)
+    landMarkedChildren[
+      getLandmark(child.props, React.Children.count(props.children))
+    ].push(<Landmark variation={child.props.variation}>{child}</Landmark>)
   })
 
   return (
@@ -26,18 +60,7 @@ export const Landmarks = (props: any) => {
       <header role="banner">{landMarkedChildren.header}</header>
       <main role="main">{landMarkedChildren.main}</main>
       <aside>{landMarkedChildren.aside}</aside>
-      <footer>{landMarkedChildren.footer}</footer>
+      <footer role="contentinfo">{landMarkedChildren.footer}</footer>
     </Fragment>
   )
-}
-
-export const Landmark = (props: any) => {
-  const label = 'Landmark-section'
-  const css = systemCss({
-    ncss: { label, ...props.ncss },
-  })
-  const attrs = {
-    css,
-  }
-  return jsx('section', attrs, props.children)
 }
--- packages/wheelroom-io/src/sections/sections.tsx
+++ packages/wheelroom-io/src/sections/sections.tsx
@@ -1,34 +1,23 @@
-import { landMarkType } from 'gatsby-theme-wheelroom'
 import React from 'react'
-import { Landmarks, Landmark } from '../lib/landmarks'
+import { Landmarks } from '../lib/landmarks'
 import { SectionProps } from '../sections/section-props'
 import { PageSection } from '../components/page-section/page-section'
 
 export interface SectionMap {
-  [contentfulSectionName: string]: {
-    element: any
-    landMark: landMarkType
-  }
+  [contentfulSectionName: string]: any
 }
 
-// This object gives us a simple string to component conversion. The object also
-// defines the landmarks. Because of the way landmarks are organized they have
-// an influence on the section order. See landmarks.jsx for more info on this.
-//
-const sectionMap = {
-  ContentfulPageSection: {
-    element: PageSection,
-    landMark: 'main',
-  },
-} as SectionMap
+const sectionMap: SectionMap = {
+  ContentfulPageSection: PageSection,
+}
 
 export const Sections = (props: any) => {
   const sectionList = [] as JSX.Element[]
   props.sections.forEach((section: any, index: number) => {
-    const Section = sectionMap[section.__typename].element
-    const landMark = sectionMap[section.__typename].landMark
+    const Section = sectionMap[section.__typename]
     const sectionProps = {
       index,
+      key: index,
       locale: props.locale,
       namedPaths: props.namedPaths,
 
@@ -38,25 +27,8 @@ export const Sections = (props: any) => {
       ...section,
     } as SectionProps
 
-    sectionList.push(
-      <Landmark landMark={landMark} key={index}>
-        <Section {...sectionProps} />
-      </Landmark>
-    )
+    sectionList.push(<Section {...sectionProps} />)
   })
 
-  // Add proper landmarks (header, main and footer) around sections
   return <Landmarks>{sectionList}</Landmarks>
 }
-
-/**
- *
- * if (index=0 && (variation='hero' || hasNavigation)) => <header role="banner">
- *
- * if (index=last && hasNavigation) => <footer role="contentinfo">
- *
- * all others are <main role="main">
- *
- * add variation prop to Landmark and use this for css label
- *
- */
