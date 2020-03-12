e0b3a4c241fca72b40e0d34e0241e8a0a26ff106
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-12 20:51:32 +0100

    fix: remove console log

--- packages/wheelroom-io/src/page-template.tsx
+++ packages/wheelroom-io/src/page-template.tsx
@@ -24,8 +24,6 @@ const PageTemplate = (props: any) => {
   // Page preview admin module
   const [previewPage, setPreviewPage] = useState()
   useFetchPreviewPage(setPreviewPage)
-  console.log('page', props.data.page)
-  console.log('previewPage', previewPage)
   const page: PageProps = previewPage || props.data.page
 
   if (!page.sections) {
