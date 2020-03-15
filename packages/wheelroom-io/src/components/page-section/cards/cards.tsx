2a80609a5ccad1140dea95daaf5c4ecef8766414
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-03-15 16:40:56 +0100

    fix: remove if condition

--- packages/wheelroom-io/src/components/page-section/cards/cards.tsx
+++ packages/wheelroom-io/src/components/page-section/cards/cards.tsx
@@ -4,38 +4,36 @@ import { Box, Container } from '../../../core/elements/grid'
 import { TopicProps } from '../../topic'
 
 export interface CardsProps {
-  topics?: TopicProps[]
+  topics: TopicProps[]
 }
 
 export const Cards = (props: CardsProps) => {
-  if (props.topics) {
-    const card = props.topics.map((topic, index) => (
-      <Card key={index} topic={topic} />
-    ))
-    return (
-      <Fragment>
-        <Box
-          is="div"
+  const card = props.topics.map((topic, index) => (
+    <Card key={index} topic={topic} />
+  ))
+  return (
+    <Fragment>
+      <Box
+        is="div"
+        ncss={{
+          label: 'Wrapper',
+          bg: 'bg',
+          py: 8,
+        }}
+      >
+        <Container
           ncss={{
-            label: 'Wrapper',
-            bg: 'bg',
-            py: 8,
+            label: 'Container',
+            maxWidth: '1280px',
+            m: 'auto',
+            justifyContent: 'center',
+            px: [2, 5, 7],
+            w: 1,
           }}
         >
-          <Container
-            ncss={{
-              label: 'Container',
-              maxWidth: '1280px',
-              m: 'auto',
-              justifyContent: 'center',
-              px: [2, 5, 7],
-              w: 1,
-            }}
-          >
-            {card}
-          </Container>
-        </Box>
-      </Fragment>
-    )
-  }
+          {card}
+        </Container>
+      </Box>
+    </Fragment>
+  )
 }
