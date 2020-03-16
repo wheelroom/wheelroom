2204099535ad7c60b18cef8c0a700da85542a7c8
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-16 21:58:32 +0100

    Merge branch 'master' of github.com:wheelroom/wheelroom-io

--- packages/wheelroom-io/src/components/page-section/page-section-images-var.tsx
+++ packages/wheelroom-io/src/components/page-section/page-section-images-var.tsx
@@ -9,8 +9,15 @@
 import React from 'react'
 import { PageSectionProps } from './page-section'
 import { NotImplemented } from '../../lib/not-implemented'
+import { getPageSectionInfo } from '../../lib/get-page-section-info'
+import { Images } from './images/images'
 
 export const PageSectionImagesVar = (props: PageSectionProps) => {
+  const info = getPageSectionInfo(props)
+  if (info.hasTopic) {
+    return <Images topics={props.topics} />
+  }
+
   /**
    * If you did not return a view above, Wheelroom will display this
    * notification
