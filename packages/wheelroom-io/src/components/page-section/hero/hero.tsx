be480448378bf47d5401a02eaa16f73b89562f99
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-18 20:54:55 +0100

    feat: convert hero to topic

--- packages/wheelroom-io/src/components/page-section/hero/hero.tsx
+++ packages/wheelroom-io/src/components/page-section/hero/hero.tsx
@@ -1,16 +1,9 @@
 import React, { Fragment } from 'react'
 import { heading1Style } from '../../../core/styles/heading'
 import { paragraphHeroStyle } from '../../../core/styles/paragraph'
-import { Box, Container } from '../../../core/elements/grid'
-import { H1 } from '../../../core/elements/heading'
-import { Paragraph } from '../../../core/elements/paragraph'
-import { Image } from '../../../core/elements/image'
-import { TopicProps } from '../../topic'
-import { Action } from '../../action'
-import { IconMap } from '../../../svg/feather/iconMap'
+import { Box } from '../../../core/elements/grid'
+import { TopicProps, Topic } from '../../topic'
 import { buttonPrimaryStyle } from '../../../core/styles/button'
-
-const iconStyle = { color: 'text', w: [1 / 10] }
 
 const heroActionStyle = {
   ...buttonPrimaryStyle,
@@ -25,9 +18,6 @@ export interface HeroProps {
 }
 
 export const Hero = (props: HeroProps) => {
-  const Icon = IconMap[props.topic.icon || 'aap']
-  const topicHasAction =
-    Array.isArray(props.topic.actions) && props.topic.actions.length > 0
   return (
     <Fragment>
       <Box
@@ -36,56 +26,37 @@ export const Hero = (props: HeroProps) => {
           label: 'Wrapper',
         }}
       >
-        <Container
-          ncss={{
-            label: 'Container',
+        <Topic
+          {...props.topic}
+          topicWrapperStyle={{
             position: 'relative',
             overflow: 'hidden',
             w: 1,
           }}
-        >
-          <Box
-            ncss={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              bottom: 0,
-              right: 0,
-              zIndex: -1,
-            }}
-          >
-            <Image image={props.topic.image} objectFit="cover" height="100%" />
-          </Box>
-          <Container
-            ncss={{
-              label: 'Container',
-              maxWidth: '1280px',
-              m: 'auto',
-              px: [2, 5, 7],
-              w: 1,
-            }}
-          >
-            <Box
-              ncss={{
-                maxWidth: '480px',
-                py: 9,
-                my: 9,
-                w: 1,
-              }}
-            >
-              <Icon strokeWidth={3} ncss={iconStyle} />
-              <H1 ncss={{ ...heading1Style, mb: 0, color: 'caviar' }}>
-                {props.topic.heading}
-              </H1>
-              <Paragraph ncss={{ ...paragraphHeroStyle, my: 3 }}>
-                {props.topic.abstract && props.topic.abstract.abstract}
-              </Paragraph>
-              {topicHasAction && (
-                <Action {...props.topic.actions[0]} ncss={heroActionStyle} />
-              )}
-            </Box>
-          </Container>
-        </Container>
+          imageWrapperStyle={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            zIndex: -1,
+          }}
+          contentWrapperStyle={{
+            maxWidth: '1280px',
+            m: 'auto',
+            px: [2, 5, 7],
+            w: 1,
+          }}
+          headerWrapperStyle={{
+            maxWidth: '480px',
+            py: 9,
+            my: 9,
+            w: 1,
+          }}
+          headingStyle={{ ...heading1Style, mb: 0, color: 'caviar' }}
+          paragraphStyle={{ ...paragraphHeroStyle, my: 3 }}
+          buttonStyle={heroActionStyle}
+        />
       </Box>
     </Fragment>
   )
