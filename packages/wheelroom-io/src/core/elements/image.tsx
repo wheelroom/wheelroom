a9cd75f95a08d944032f5ff266c64fa278f02174
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-19 15:26:46 +0100

    fix: cleanup img

--- packages/wheelroom-io/src/core/elements/image.tsx
+++ packages/wheelroom-io/src/core/elements/image.tsx
@@ -2,105 +2,71 @@
 import { jsx } from '@emotion/core'
 import { systemCss, ThemeId } from '../../styled-system/system-css'
 import { useGetCurrentThemeId } from '@jacco-meijer/admin-theme-switcher'
-import { styleProp, FluidImage } from './types'
+import { FluidImage } from './types'
+import {
+  commonImagePictureStyle,
+  commonImageImgStyle,
+  commonImageFigcaptionStyle,
+} from '../styles/image'
 
 export interface ImageProps {
-  '&:hover'?: any
-  description?: string
-  title?: string
-  border?: string
-  borderRadius?: string | string[]
-  clipPath?: string
-  height?: styleProp
-  width?: any
-  /** fluid image object, see defaultProps */
+  alt: string
+  figcaptionNcss: any
   image?: FluidImage
-  maxHeight?: styleProp
-  minHeight?: styleProp
-  my?: styleProp
-  mb?: styleProp
-  padding?: styleProp
-  margin?: styleProp
-  /** like the css property */
-  objectFit?: string
-  /** like the css property */
-  transition?: string
-  caption?: boolean
+  imgNcss: any
+  includeFigcaption: boolean
+  pictureNcss: any
+  title: string
 }
+
+const defaultFluidImage = {
+  description: 'no description',
+  fluid: {
+    sizes: '',
+    src: '//placehold.it/1024',
+    srcSet: '',
+  },
+  title: 'No alternate text available',
+} as FluidImage
 
 export const Image = (props: ImageProps) => {
   const currentThemeId = useGetCurrentThemeId() as ThemeId
 
-  const pictureStyles = {
-    '&:hover': props['&:hover'],
-    border: props.border,
-    display: 'block',
-    height: props.height,
-    maxHeight: props.maxHeight,
-    minHeight: props.minHeight,
-    my: props.my,
-    mb: props.mb,
-    p: props.padding,
-    m: props.margin,
-    transition: props.transition,
-    width: props.width,
-  }
-
-  const defaultFluidImage = {
-    description: 'no description',
-    fluid: {
-      sizes: '',
-      src: '//placehold.it/1024',
-      srcSet: '',
-    },
-    title: 'No alternate text available',
-  } as FluidImage
-
   const fluidImage = props.image || defaultFluidImage
 
-  const imgStyles = {
-    borderRadius: props.borderRadius,
-    clipPath: props.clipPath,
-    display: 'block',
-    height: props.height || 'auto',
-    objectFit: props.objectFit,
-    width: '100%',
-  }
-
   const imgAttrs = {
-    alt: fluidImage.title || 'No alternate text available',
-    title: fluidImage.title || null,
+    alt: fluidImage.description || props.alt || 'No alternate text available',
+    title: fluidImage.title || props.title || 'No title',
     sizes: fluidImage.fluid.sizes,
     src: fluidImage.fluid.src,
     srcSet: fluidImage.fluid.srcSet,
   }
 
-  const imgCaptionStyles = {
-    fontFamily: 'text',
-    color: 'text',
-    fontSize: [1, 2],
-    fontWeight: 2,
-    my: 2,
-  }
-
-  const imgCaption = {
-    show: props.caption,
-  }
-
-  const ImageCaption = (imgCaption: any) => {
-    if (imgCaption.show && fluidImage.description)
-      return (
-        <figcaption css={systemCss({ ncss: imgCaptionStyles }, currentThemeId)}>
-          {fluidImage.description}
-        </figcaption>
-      )
-    else return null
-  }
+  const figcaptionNcss = props.figcaptionNcss || {}
+  const imgNcss = props.imgNcss || {}
+  const pictureNcss = props.pictureNcss || {}
 
   return (
-    <picture css={systemCss({ ncss: pictureStyles }, currentThemeId)}>
-      <img {...imgAttrs} css={systemCss({ ncss: imgStyles })} />
-      <ImageCaption {...imgCaption} />
+    <picture
+      css={systemCss(
+        { ncss: { ...commonImagePictureStyle, ...pictureNcss } },
+        currentThemeId
+      )}
+    >
+      <img
+        {...imgAttrs}
+        css={systemCss({ ncss: { ...commonImageImgStyle, ...imgNcss } })}
+      />
+      {props.includeFigcaption && (
+        <figcaption
+          css={systemCss(
+            { ncss: { ...commonImageFigcaptionStyle, ...figcaptionNcss } },
+            currentThemeId
+          )}
+        >
+          {imgAttrs.alt}
+        </figcaption>
+      )}
     </picture>
   )
 }
--- packages/wheelroom-io/src/core/styles/image.ts
+++ packages/wheelroom-io/src/core/styles/image.ts
@@ -1,1 +1,17 @@
-export const commonImageStyle = {}
+export const commonImageImgStyle = {
+  display: 'block',
+  height: 'auto',
+  objectFit: 'cover',
+  width: '100%',
+}
+export const commonImagePictureStyle = {
+  display: 'block',
+}
+
+export const commonImageFigcaptionStyle = {
+  fontFamily: 'text',
+  color: 'text',
+  fontSize: [1, 2],
+  fontWeight: 2,
+  my: 2,
+}
