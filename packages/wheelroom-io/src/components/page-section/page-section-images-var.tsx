f51b633efef6cab226e10d54d308f2c164861633
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-19 16:08:47 +0100

    chore: remove images

--- packages/wheelroom-io/src/components/page-section/page-section-images-var.tsx
+++ packages/wheelroom-io/src/components/page-section/page-section-images-var.tsx
@@ -10,13 +10,10 @@ import React from 'react'
 import { PageSectionProps } from './page-section'
 import { NotImplemented } from '../../lib/not-implemented'
 import { getPageSectionInfo } from '../../lib/get-page-section-info'
-import { Images } from './images/images'
 
 export const PageSectionImagesVar = (props: PageSectionProps) => {
   const info = getPageSectionInfo(props)
-  if (info.hasTopic) {
-    return <Images topics={props.topics} />
-  }
+  console.log('Not implemented', info)
 
   /**
    * If you did not return a view above, Wheelroom will display this
