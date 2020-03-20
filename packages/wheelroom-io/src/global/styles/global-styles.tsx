15de4dc5e6f01c0953eee272071459a113c158d4
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-20 15:23:35 +0100

    wip: global styles

--- packages/wheelroom-io/src/global/styles/global-styles.tsx
+++ packages/wheelroom-io/src/global/styles/global-styles.tsx
@@ -1,3 +1,28 @@
 export const globalStyles = {
-  body: 'margin: 0',
+  body: { margin: 0 },
+  html: { lineHeight: 1.15 },
+  small: {
+    fontSize: '80%',
+  },
+  'sub, sup': {
+    fontSize: '75%',
+    lineHeight: '0',
+    position: 'relative',
+    verticalAlign: 'baseline',
+  },
+  sub: {
+    bottom: '-0.25em',
+  },
+  sup: {
+    top: '-0.5em',
+  },
+  'button, input, optgroup, select, textarea': {
+    fontFamily: 'inherit',
+    fontSize: '100%',
+    lineHeight: '1.15',
+    margin: '0',
+  },
+  'button, input': {
+    overflow: 'visible',
+  },
 }
