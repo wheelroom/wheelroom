a544602a3702f4739ee53b69945175349fa91ed4
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-03-21 17:04:02 +0100

    chore: rich text video styling

--- packages/wheelroom-io/src/core/styles/video.ts
+++ packages/wheelroom-io/src/core/styles/video.ts
@@ -1,9 +1,8 @@
 export const commonVideoStyle = {
   label: 'video',
-  boxSizing: 'border-box',
-  display: 'block',
   height: 'auto',
   width: '100%',
+  my: [4, 5],
 }
 
 export const commonVideoFigcaptionStyle = {
@@ -12,5 +11,6 @@ export const commonVideoFigcaptionStyle = {
   color: 'text',
   fontSize: [1, 2],
   fontWeight: 3,
-  my: 2,
+  mt: 0,
+  mb: [4, 5],
 }
