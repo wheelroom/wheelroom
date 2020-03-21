e201f148b31fdeab84a65acacd6a5ce6109fcf3d
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-03-21 22:35:16 +0100

    chore: refactor featured variation styling

--- packages/wheelroom-io/src/components/page-section/featured/featured-multiple-topics.tsx
+++ packages/wheelroom-io/src/components/page-section/featured/featured-multiple-topics.tsx
@@ -2,37 +2,19 @@ import React from 'react'
 import { TopicProps, Topic } from '../../topic'
 import { PageSectionInfo } from '../../../lib/get-page-section-info'
 import { Box, ContainerMaxWidth, Flex } from '../../../core/elements/grid'
-import { Image } from '../../../core/elements/image'
 import { TopicOption } from '../page-section'
 
 const topicWrapperStyle = {
   label: 'topic-wrapper',
   flexDirection: ['column', 'column', 'row'],
-  alignItems: ['center'],
-}
-
-const imageWrapperStyle = {
-  label: 'topic-image-wrapper',
-  flexDirection: 'column',
-  w: [1, 1, 1 / 3],
-  mb: [3, 3, 0],
-}
-
-const contentWrapperStyle = {
-  label: 'topic-content-wrapper',
-  flexDirection: 'row',
   flexWrap: 'wrap',
-  w: [1, 1, 2 / 3],
 }
 
 /**
  *
- * Requires two or more topics
+ * Requires four or more topics
  *
- * - Two topics: the image of the first topic is displayed, the two topics ared
- *   shown next to the image
- * - Four topics: the image of the first topic is displayed, the four topics
- *   are shown next to the image
+ * - Four topics: the four topics
  *
  */
 
@@ -47,7 +29,6 @@ export const FeaturedMultipleTopics = (props: FeaturedMultipleTopicsProps) => {
   if (!(props.info.hasTopic || props.info.hasText)) {
     return null
   }
-  const image = props.topics[0].image
   return (
     <Box
       is="div"
@@ -63,43 +44,28 @@ export const FeaturedMultipleTopics = (props: FeaturedMultipleTopicsProps) => {
         }}
       >
         <Flex ncss={topicWrapperStyle}>
-          <Flex ncss={imageWrapperStyle}>
-            <Image image={image} pictureNcss={{ p: 3 }} />
-          </Flex>
-          <Flex ncss={contentWrapperStyle}>
-            {props.topics.map((topic: TopicProps, index: number) => (
-              <Topic
-                key={index}
-                {...topic}
-                image={undefined}
-                topicOptions={props.topicOptions}
-                topicWrapperStyle={{
-                  flexDirection: ['column', 'column', 'row'],
-                  alignItems: 'baseline',
-                  w: props.topics.length === 4 ? [1, 1, 1 / 2] : 1,
-                }}
-                imageWrapperStyle={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  w: 1,
-                  mb: [3, 3, 0],
-                }}
-                contentWrapperStyle={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  w: 1,
-                }}
-                headerWrapperStyle={{
-                  mx: 3,
-                }}
-                useHeading="h3"
-                actionWrapperStyle={{
-                  display: 'flex',
-                  mx: 3,
-                }}
-              />
-            ))}
-          </Flex>
+          {props.topics.map((topic: TopicProps, index: number) => (
+            <Topic
+              key={index}
+              {...topic}
+              topicOptions={props.topicOptions}
+              topicWrapperStyle={{
+                flexDirection: 'column',
+                w: [1, 1, 1 / 2],
+              }}
+              imageWrapperStyle={{
+                display: 'flex',
+                p: 3,
+                w: 1,
+              }}
+              contentWrapperStyle={{
+                display: 'flex',
+                flexDirection: 'column',
+                w: 1,
+              }}
+              useHeading="h3"
+            />
+          ))}
         </Flex>
       </ContainerMaxWidth>
     </Box>
--- packages/wheelroom-io/src/components/page-section/featured/featured-two-topics.tsx
+++ packages/wheelroom-io/src/components/page-section/featured/featured-two-topics.tsx
@@ -2,52 +2,33 @@ import React from 'react'
 import { TopicProps, Topic } from '../../topic'
 import { PageSectionInfo } from '../../../lib/get-page-section-info'
 import { Box, ContainerMaxWidth, Flex } from '../../../core/elements/grid'
-import { Image } from '../../../core/elements/image'
 import { TopicOption } from '../page-section'
 
 const topicWrapperStyle = {
   label: 'topic-wrapper',
   flexDirection: ['column', 'column', 'row'],
-  alignItems: ['center'],
-}
-
-const imageWrapperStyle = {
-  label: 'topic-image-wrapper',
-  flexDirection: 'column',
-  w: [1, 1, 1 / 3],
-  mb: [3, 3, 0],
-}
-
-const contentWrapperStyle = {
-  label: 'topic-content-wrapper',
-  flexDirection: 'row',
   flexWrap: 'wrap',
-  w: [1, 1, 2 / 3],
 }
 
 /**
  *
- * Requires two or more topics
+ * Requires two topics
  *
- * - Two topics: the image of the first topic is displayed, the two topics ared
- *   shown next to the image
- * - Four topics: the image of the first topic is displayed, the four topics
- *   are shown next to the image
+ * - Two topics:
  *
  */
 
-export interface FeaturedMultipleTopicsProps {
+export interface FeaturedTwoTopicsProps {
   info: PageSectionInfo
   topics: TopicProps[]
   topicOptions: TopicOption[]
 }
 
-export const FeaturedTwoTopics = (props: FeaturedMultipleTopicsProps) => {
+export const FeaturedTwoTopics = (props: FeaturedTwoTopicsProps) => {
   // Featured needs topics or text, return null (render nothing) otherwise
   if (!(props.info.hasTopic || props.info.hasText)) {
     return null
   }
-  const image = props.topics[0].image
   return (
     <Box
       is="div"
@@ -63,43 +44,29 @@ export const FeaturedTwoTopics = (props: FeaturedMultipleTopicsProps) => {
         }}
       >
         <Flex ncss={topicWrapperStyle}>
-          <Flex ncss={imageWrapperStyle}>
-            <Image image={image} pictureNcss={{ p: 3 }} />
-          </Flex>
-          <Flex ncss={contentWrapperStyle}>
-            {props.topics.map((topic: TopicProps, index: number) => (
-              <Topic
-                key={index}
-                {...topic}
-                image={undefined}
-                topicOptions={props.topicOptions}
-                topicWrapperStyle={{
-                  flexDirection: ['column', 'column', 'row'],
-                  alignItems: 'baseline',
-                  w: props.topics.length === 4 ? [1, 1, 1 / 2] : 1,
-                }}
-                imageWrapperStyle={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  w: 1,
-                  mb: [3, 3, 0],
-                }}
-                contentWrapperStyle={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  w: 1,
-                }}
-                headerWrapperStyle={{
-                  mx: 3,
-                }}
-                useHeading="h3"
-                actionWrapperStyle={{
-                  display: 'flex',
-                  mx: 3,
-                }}
-              />
-            ))}
-          </Flex>
+          {props.topics.map((topic: TopicProps, index: number) => (
+            <Topic
+              key={index}
+              {...topic}
+              topicOptions={props.topicOptions}
+              topicWrapperStyle={{
+                flexDirection: ['column', 'row'],
+                w: 1,
+              }}
+              imageWrapperStyle={{
+                display: 'flex',
+                  alignItems: 'center',
+                p: 3,
+                w: 1,
+              }}
+              contentWrapperStyle={{
+                display: 'flex',
+                flexDirection: 'column',
+                w: 1,
+              }}
+              useHeading="h3"
+            />
+          ))}
         </Flex>
       </ContainerMaxWidth>
     </Box>
