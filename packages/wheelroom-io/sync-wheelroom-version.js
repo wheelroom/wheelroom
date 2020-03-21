b4a920e9fcdd23739904ed14b1d1f2c150d29b58
Jacco Meijer <jacco@jaccomeijer.nl>
2020-03-21 17:42:30 +0100

    feat: add sync-wheelroom-version

---
+++ packages/wheelroom-io/sync-wheelroom-version.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict'
+
+const readPkg = require('read-pkg')
+const writePkg = require('write-pkg')
+
+const scriptName = 'sync-wheelroom-version'
+
+async function syncVersion() {
+  try {
+    const packageJson = await readPkg()
+    packageJson.wheelroom.version = packageJson.version
+    await writePkg(packageJson)
+  } catch (err) {
+    process.exit(1)
+  }
+}
+
+;(async () => {
+  console.log(`${scriptName}: running...`)
+  await syncVersion()
+  console.log(`${scriptName}: done`)
+})()
