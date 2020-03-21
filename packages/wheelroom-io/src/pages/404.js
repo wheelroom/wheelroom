f76f3b0da7ed396f972a8b63dbcc15f019609e96
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-03-21 18:52:34 +0100

    chore: style 404 page

--- packages/wheelroom-io/src/pages/404.js
+++ packages/wheelroom-io/src/pages/404.js
@@ -1,10 +1,29 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import { Box, ContainerMaxWidth } from '../core/elements/grid'
+import { H1 } from '../core/elements/heading'
+import { Paragraph } from '../core/elements/paragraph'
+import { commonHeadingStyle, heading1Style } from '../core/styles/heading'
+import { commonParagraphStyle } from '../core/styles/paragraph'
 
 const NotFoundPage = () => (
-  <Fragment>
-    <h1>NOT FOUND</h1>
-    <p>So sorry, this path doesn&#39;t exist!</p>
-  </Fragment>
+  <Box
+    is="div"
+    ncss={{
+      label: 'wrapper',
+      py: 8,
+    }}
+  >
+    <ContainerMaxWidth>
+      <Box>
+        <H1 ncss={{ ...commonHeadingStyle, ...heading1Style, color: 'caviar' }}>
+          404. That’s an error.
+        </H1>
+        <Paragraph ncss={{ ...commonParagraphStyle, color: 'caviar' }}>
+          So sorry, this path doesn&#39;t exist!
+        </Paragraph>
+      </Box>
+    </ContainerMaxWidth>
+  </Box>
 )
 
 export default NotFoundPage
