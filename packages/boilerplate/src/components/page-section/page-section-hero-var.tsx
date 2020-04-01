4833824ddf2bcab2fdf0d9080e5631d3caa0e835
Jacco Meijer <jacco@jaccomeijer.nl>
2020-04-01 17:43:27 +0200

    fix: hero styling

--- packages/boilerplate/src/components/page-section/page-section-hero-var.tsx
+++ packages/boilerplate/src/components/page-section/page-section-hero-var.tsx
@@ -11,14 +11,12 @@ import { PageSectionProps } from './page-section'
 import { NotImplemented } from '../../core/lib/not-implemented'
 import { getPageSectionInfo } from '../../core/lib/get-page-section-info'
 import { HeroDisplay } from '../../core/displays/hero-display'
-import { headlineDisplayStyleTree } from '../../styles/style-trees/headline-display-style-tree'
+import { heroDisplayStyleTree } from '../../styles/style-trees/hero-display-style-tree'
 
 export const PageSectionHeroVar = (props: PageSectionProps) => {
   const pageSectionInfo = getPageSectionInfo(props)
   if (pageSectionInfo.hasTopic) {
-    return (
-      <HeroDisplay pageSection={props} styleTree={headlineDisplayStyleTree} />
-    )
+    return <HeroDisplay pageSection={props} styleTree={heroDisplayStyleTree} />
   }
 
   /**
