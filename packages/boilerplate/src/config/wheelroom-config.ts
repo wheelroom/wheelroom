a48bd0bc4aac4d18703b26bc6a6e14b0d7880410
Jacco Meijer <jacco@jaccomeijer.nl>
2020-04-05 20:39:06 +0200

    refactor: better file names

--- packages/boilerplate/src/config/wheelroom-config.ts
+++ packages/boilerplate/src/config/wheelroom-config.ts
@@ -1,8 +1,8 @@
 import { WheelroomConfig } from '@wheelroom/wheelroom'
-import { configCommonFields } from './wheelroom/config-common-fields'
+import { configCommonFields } from './wheelroom/common-fields'
 import { models } from './wheelroom/models'
-import { configFieldDefaults } from './wheelroom/config-field-defaults'
-import { configPluginOptions } from './wheelroom/config-plugin-options'
+import { configFieldDefaults } from './wheelroom/field-defaults'
+import { configPluginOptions } from './wheelroom/plugin-options'
 
 export const config: WheelroomConfig = {
   commonFields: configCommonFields,
