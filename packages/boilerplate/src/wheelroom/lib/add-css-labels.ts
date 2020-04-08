555d0b250334b7c684e89435d6c07cb5b4b925e9
Jacco Meijer <jacco@jaccomeijer.nl>
2020-04-08 17:52:16 +0200

    fix: css labels

--- packages/boilerplate/src/wheelroom/lib/add-css-labels.ts
+++ packages/boilerplate/src/wheelroom/lib/add-css-labels.ts
@@ -1,11 +1,11 @@
 import { TreeStyle } from './tree-style'
 
 export const addCssLabels = (prefix: string, treeStyle: TreeStyle) => {
-  return
   Object.entries(treeStyle).forEach(([name, value]) => {
     if (value instanceof Object && !Array.isArray(value)) {
       addCssLabels(`${prefix}-${name}`, value)
     }
   })
-  treeStyle.label = prefix.replace(':', '')
+
+  treeStyle.label = prefix.replace(/:|&/g, '')
 }
