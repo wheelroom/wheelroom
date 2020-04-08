cc11de8713990f0ecef90330f6453263e2f785f8
Jacco Meijer <jacco@jaccomeijer.nl>
2020-04-08 19:29:50 +0200

    docs: overwriteWith example with deepMerge

--- packages/boilerplate/src/wheelroom/lib/deep-merge.ts
+++ packages/boilerplate/src/wheelroom/lib/deep-merge.ts
@@ -2,6 +2,17 @@ import deepmerge from 'deepmerge'
 
 const overwriteMerge = (destinationArray: any, sourceArray: any) => sourceArray
 
+/**
+ *
+ * Use this to apply custom styling. The first base object contains styling from
+ * the wheelroom core. The second object contains the styling to overwrite.
+ *
+ * Note that arrays are replaced, while objects are merged.
+ *
+ * Example overwriteWith: { topic: { wrapper: { p: 3 } } }
+ *
+ */
+
 export const deepMerge = <T>(base: T, overwriteWith: T): T => {
   return deepmerge(base, overwriteWith, { arrayMerge: overwriteMerge })
 }
