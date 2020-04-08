3705d42601c92589ac9eea79819edf4c3704196a
Jacco Meijer <jacco@jaccomeijer.nl>
2020-04-08 17:20:49 +0200

    fix: types

--- packages/boilerplate/src/wheelroom/lib/add-css-labels.ts
+++ packages/boilerplate/src/wheelroom/lib/add-css-labels.ts
@@ -1,6 +1,6 @@
 import { TreeStyle } from './tree-style'
 
-const addLabel = (prefix: string, treeStyle: TreeStyle): TreeStyle => {
+const addLabel = <T>(prefix: string, treeStyle: TreeStyle): T => {
   Object.entries(treeStyle).forEach(([name, value]) => {
     if (value instanceof Object && !Array.isArray(value)) {
       addLabel(`${prefix}-${name}`, value)
@@ -8,13 +8,10 @@ const addLabel = (prefix: string, treeStyle: TreeStyle): TreeStyle => {
   })
   treeStyle.label = prefix.replace(':', '')
   console.log(treeStyle)
-  return treeStyle
+  return treeStyle as T
 }
 
-export const addCssLabels = (
-  prefix: string,
-  treeStyle: TreeStyle
-): TreeStyle => {
+export const addCssLabels = <T>(prefix: string, treeStyle: TreeStyle): T => {
   console.log(treeStyle)
   return addLabel(prefix, treeStyle)
 }
