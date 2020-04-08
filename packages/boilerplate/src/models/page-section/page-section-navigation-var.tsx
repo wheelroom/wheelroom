516c262f158a78f0955f45b655a2c20d0e3153f4
Jacco Meijer <jacco@jaccomeijer.nl>
2020-04-08 12:50:02 +0200

    chore: remove example

--- packages/boilerplate/src/models/page-section/page-section-navigation-var.tsx
+++ packages/boilerplate/src/models/page-section/page-section-navigation-var.tsx
@@ -14,21 +14,14 @@ import { navigationHeaderTreeStyle } from '../../wheelroom/trees/page-section/na
 import { navigationFooterTreeStyle } from '../../wheelroom/trees/page-section/navigation/footer/page-section-navigation-footer-tree-style'
 import { PageSectionNavigationHeaderTree } from '../../wheelroom/trees/page-section/navigation/header/page-section-navigation-header-tree'
 import { PageSectionNavigationFooterTree } from '../../wheelroom/trees/page-section/navigation/footer/page-section-navigation-footer-tree'
-import { SvgStack } from '../../svg/figma'
 
 export const PageSectionNavigationVar = (props: PageSectionProps) => {
   const info = getPageSectionInfo(props)
   if (info.hasNavigation && info.index === 0) {
-    navigationHeaderTreeStyle.menu.navLogo.container = {
-      ...navigationHeaderTreeStyle.menu.navLogo.container,
-      bg: 'azure',
-    }
-
     return (
       <PageSectionNavigationHeaderTree
         pageSection={props}
         treeStyle={navigationHeaderTreeStyle}
-        useLogoElement={<SvgStack ncss={{ w: '33px' }} />}
       />
     )
   }
