bc20d41d4c9a882117bd760cbb277bbf859ba136
Jacco Meijer <jacco@jaccomeijer.nl>
2020-04-13 17:13:31 +0200

    chore: add image and video

--- packages/boilerplate/src/models/page-section/page-section-image-var.tsx
+++ packages/boilerplate/src/models/page-section/page-section-image-var.tsx
@@ -9,35 +9,41 @@
 import React from 'react'
 import { PageSectionProps } from './page-section'
 import { NotImplemented } from '../../wheelroom/lib/not-implemented'
+import { getPageSectionInfo } from '../../wheelroom/lib/get-page-section-info'
+import { PageSectionUnicorn } from '../../wheelroom/wheels/page-section/unicorn/page-section-unicorn'
+import { deepMerge } from '../../wheelroom/lib/deep-merge'
+import { topicPreset } from '../../wheelroom/wheels/topic/presets/topic-preset'
 import { Wheel } from '../../wheelroom/wheels/types'
 import { elementPresets } from '../../themes/yosemite/element-presets'
 import { yosemiteLight } from '../../themes/yosemite/yosemite-light'
 import { styledSystemConfig } from '../../themes/yosemite/styled-system-config'
+import { pageSectionImagePreset } from '../../wheelroom/wheels/page-section/image/page-section-image-preset'
 
 export const PageSectionImageVar = (props: PageSectionProps) => {
-  // const pageSectionInfo = getPageSectionInfo(props)
+  const pageSectionInfo = getPageSectionInfo(props)
 
-  // const treeStyle = deepMerge(
-  //   { topic: topicPreset },
-  //   { ...pageSectionImagePreset }
-  // )
-
-  // if (pageSectionInfo.hasTopic) {
-  //   return (
-  //     <PageSectionImage
-  //       containerStyle="fluid"
-  //       pageSection={props}
-  //       treeStyle={treeStyle}
-  //     />
-  //   )
-  // }
-
+  const style = deepMerge({ topic: topicPreset }, { ...pageSectionImagePreset })
   const wheel: Wheel = {
-    style: {},
+    style,
     elementPresets,
     theme: yosemiteLight,
     styledSystemConfig,
   }
 
+  if (pageSectionInfo.hasTopic) {
+    return (
+      <PageSectionUnicorn
+        topicProps={{
+          pageSectionActions: props.actions,
+          pageSectionInfo,
+          wheel,
+        }}
+        containerStyle="fluid"
+        maxTopics={2}
+        pageSection={props}
+        wheel={wheel}
+      />
+    )
+  }
   return <NotImplemented pageSection={props} wheel={wheel} />
 }
--- packages/boilerplate/src/models/page-section/page-section-video-var.tsx
+++ packages/boilerplate/src/models/page-section/page-section-video-var.tsx
@@ -9,37 +9,40 @@
 import React from 'react'
 import { PageSectionProps } from './page-section'
 import { NotImplemented } from '../../wheelroom/lib/not-implemented'
+import { getPageSectionInfo } from '../../wheelroom/lib/get-page-section-info'
+import { PageSectionUnicorn } from '../../wheelroom/wheels/page-section/unicorn/page-section-unicorn'
+import { deepMerge } from '../../wheelroom/lib/deep-merge'
+import { topicPreset } from '../../wheelroom/wheels/topic/presets/topic-preset'
 import { Wheel } from '../../wheelroom/wheels/types'
 import { elementPresets } from '../../themes/yosemite/element-presets'
 import { yosemiteLight } from '../../themes/yosemite/yosemite-light'
 import { styledSystemConfig } from '../../themes/yosemite/styled-system-config'
+import { pageSectionVideoPreset } from '../../wheelroom/wheels/page-section/video/page-section-video-preset'
 
-export const PageSectionVideoVar = (props: PageSectionProps) => {
-  // const pageSectionInfo = getPageSectionInfo(props)
+export const PageSectionImageVar = (props: PageSectionProps) => {
+  const pageSectionInfo = getPageSectionInfo(props)
 
-  // const treeStyle = deepMerge(
-  //   { topic: topicPreset },
-  //   {
-  //     ...pageSectionVideoPreset,
-  //   }
-  // )
-
-  // if (pageSectionInfo.hasTopic) {
-  //   return (
-  //     <PageSectionVideo
-  //       containerStyle="fluid"
-  //       pageSection={props}
-  //       treeStyle={treeStyle}
-  //     />
-  //   )
-  // }
-
+  const style = deepMerge({ topic: topicPreset }, { ...pageSectionVideoPreset })
   const wheel: Wheel = {
-    style: {},
+    style,
     elementPresets,
     theme: yosemiteLight,
     styledSystemConfig,
   }
 
+  if (pageSectionInfo.hasTopic) {
+    return (
+      <PageSectionUnicorn
+        topicProps={{
+          pageSectionActions: props.actions,
+          pageSectionInfo,
+          wheel,
+        }}
+        containerStyle="fluid"
+        pageSection={props}
+        wheel={wheel}
+      />
+    )
+  }
   return <NotImplemented pageSection={props} wheel={wheel} />
 }
