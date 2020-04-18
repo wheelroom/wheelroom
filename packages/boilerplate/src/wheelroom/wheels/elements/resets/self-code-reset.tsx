439cb00b91b58b07ae43b9d85c1f8b09fb16c2df
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-04-18 17:51:50 +0200

    chore: refactor comments

--- packages/boilerplate/src/wheelroom/wheels/elements/resets/self-code-reset.tsx
+++ packages/boilerplate/src/wheelroom/wheels/elements/resets/self-code-reset.tsx
@@ -9,7 +9,7 @@ export const selfCodeReset: SelfCodeReset = {
     // 1. Correct the inheritance and scaling of font size in all browsers.
     // 2. Correct the odd `em` font sizing in all browsers.
     boxSizing: 'border-box',
-    fontFamily: 'monospace, monospace' /* 1 */,
-    fontSize: '1em' /* 2 */,
+    fontFamily: 'monospace, monospace', // 1
+    fontSize: '1em', // 2
   },
 }
