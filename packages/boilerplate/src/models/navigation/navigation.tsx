bb43ba279b354a62526af3627c365354591231cc
Jacco Meijer <jacco@jaccomeijer.nl>
2020-04-19 19:44:20 +0200

    chore: update ts for new fields

--- packages/boilerplate/src/models/navigation/navigation.tsx
+++ packages/boilerplate/src/models/navigation/navigation.tsx
@@ -6,10 +6,13 @@
  */
 
 import { NavigationSegmentProps } from '../navigation-segment/navigation-segment'
+import { ActionProps } from '../action';
 
 export interface NavigationProps {
   /** Gatsby fetched data */
   __typename: string
   title: string
+  brandAction: ActionProps
   segments: NavigationSegmentProps[]
+  skipToContentHeading: string
 }
