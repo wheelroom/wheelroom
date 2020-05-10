5d7c9540d6fb4c7cb3a6f3037910d10905b40dd2
Jacco Meijer <jacco@jaccomeijer.nl>
2020-05-10 21:56:28 +0200

    chore: add page sections blog and price table

---
+++ packages/boilerplate/src/models/page-section/page-section-price-table-var.tsx
@@ -0,0 +1,25 @@
+/**
+ * Component variation
+ *
+ * Component type: pageSection
+ * Variation: Divider
+ *
+ */
+
+import React from 'react'
+import { Flex } from '../../wheelroom/wheels/element/grid'
+import { getWheel } from '../../themes/themes'
+import { PageSectionProps } from '.'
+import { ThemeId } from '../../admin-resources/theme-info'
+import { Wheel } from '../../wheelroom/wheels/types'
+
+export const PageSectionPriceTableVar = (props: PageSectionProps) => {
+  const wheel: Wheel = getWheel(props.activeThemeId as ThemeId)
+  wheel.style = { ncss: {} }
+
+  return (
+    <Flex wheel={wheel} ncss={{ bg: 'amber' }}>
+      Price table
+    </Flex>
+  )
+}
--- packages/boilerplate/src/models/page-section/page-section.tsx
+++ packages/boilerplate/src/models/page-section/page-section.tsx
@@ -10,18 +10,20 @@ import { ActionProps } from '../action'
 import { getVariation } from '../../wheelroom/lib/get-variation'
 import { NavigationProps } from '../navigation'
 import { PageSectionBlockVar } from './page-section-block-var'
+import { PageSectionBlogVar } from './page-section-blog-var'
 import { PageSectionCardVar } from './page-section-card-var'
+import { PageSectionDividerVar } from './page-section-divider-var'
 import { PageSectionFeaturedVar } from './page-section-featured-var'
 import { PageSectionGalleryVar } from './page-section-gallery-var'
 import { PageSectionHeadlineVar } from './page-section-headline-var'
 import { PageSectionHeroVar } from './page-section-hero-var'
 import { PageSectionImageVar } from './page-section-image-var'
 import { PageSectionNavigationVar } from './page-section-navigation-var'
+import { PageSectionPriceTableVar } from './page-section-price-table-var'
 import { PageSectionQuoteVar } from './page-section-quote-var'
 import { PageSectionShowcaseVar } from './page-section-showcase-var'
 import { PageSectionTextVar } from './page-section-text-var'
 import { PageSectionVideoVar } from './page-section-video-var'
-import { PageSectionDividerVar } from './page-section-divider-var'
 import { SectionProps } from '../../wheelroom/sections/section-props'
 import { TextProps } from '../text'
 import { TopicOptionString } from '../../wheelroom/lib/get-topic-options'
@@ -30,6 +32,7 @@ import { TopicTableProps } from '../topic-table/topic-table'
 
 const componentList = {
   ['block-wr']: PageSectionBlockVar,
+  ['blog-wr']: PageSectionBlogVar,
   ['card-wr']: PageSectionCardVar,
   ['divider-wr']: PageSectionDividerVar,
   ['featured-wr']: PageSectionFeaturedVar,
@@ -38,6 +41,7 @@ const componentList = {
   ['hero-wr']: PageSectionHeroVar,
   ['image-wr']: PageSectionImageVar,
   ['navigation-wr']: PageSectionNavigationVar,
+  ['price-table-wr']: PageSectionPriceTableVar,
   ['quote-wr']: PageSectionQuoteVar,
   ['showcase-wr']: PageSectionShowcaseVar,
   ['text-wr']: PageSectionTextVar,
