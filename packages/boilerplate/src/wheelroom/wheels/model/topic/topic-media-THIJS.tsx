9030c7c69d10dac96a5859d92dbb6e0ad046b6ce
Jacco Meijer <jacco@jaccomeijer.nl>
2020-05-21 15:11:37 +0200

    chore: topic media prepare

---
+++ packages/boilerplate/src/wheelroom/wheels/model/topic/topic-media-THIJS.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Image } from '../../element/image'
+import { Box } from '../../element/grid'
+import { TopicProps } from '../../../../models/topic'
+import { TopicInfo } from '../../../lib/get-topic-info'
+import { PageSectionInfo } from '../../../lib/get-page-section-info'
+import { Video } from '../../element/video'
+import { Wheel } from '../../types'
+import { TopicMediaWheelStyle } from './presets/topic-media-preset'
+import { EmbedProps } from '../../../../models/embed'
+import { Html } from '../../element/html'
+import { MediaObject } from '../../element/types/media'
+
+export interface TopicMediaWheel extends Wheel {
+  style: TopicMediaWheelStyle
+}
+
+export interface TopicMediaProps {
+  /** Styling wheel */
+  wheel: TopicMediaWheel
+  /** All topic props */
+  topic: TopicProps
+  /** All embed props */
+  embed: EmbedProps
+  /** Topic info object */
+  topicInfo: TopicInfo
+  /** Page section info */
+  pageSectionInfo: PageSectionInfo
+}
+
+export const TopicMedia = (props: TopicMediaProps) => {
+  const pageSectionInfo = props.pageSectionInfo
+  const topicOptions = pageSectionInfo.topicOptions
+  const topicInfo = props.topicInfo
+  const topic = props.topic
+  let Media = null
+  let mediaProps = {}
+  if (topicInfo.hasImage && topic.media) {
+    const media: MediaObject = topic.media
+    Media = Image
+    mediaProps = {
+      includeFigcaption: false,
+      description: media.description,
+      media: topic.media,
+      wheel: { ...props.wheel, style: props.wheel.style.image },
+      title: media.title,
+    }
+  } else if (topicInfo.hasVideo && topic.media) {
+    const media: MediaObject = topic.media
+    Media = Video
+    mediaProps = {
+      includeTitle: false,
+      includeDescription: false,
+      description: media.description,
+      media: media,
+      wheel: { ...props.wheel, style: props.wheel.style.video },
+      title: media.title,
+    }
+  } else if (topicInfo.hasMediaEmbed && topic.mediaEmbed) {
+    const embed = props.topic.mediaEmbed
+    Media = Embed
+    mediaProps = {
+      wheel: { ...props.wheel, style: props.wheel.style.mediaEmbed },
+    }
+  } else if (topicInfo.hasMediaBreakpoint && topic.mediaBreakpoint) {
+    Media = <div>Media breakpoint not yet implemented</div>
+    mediaProps = {}
+  }
+
+  return (
+    <Box
+      ncss={{
+        order: topicOptions.reverseOrder ? 0 : null,
+      }}
+      wheel={props.wheel}
+    >
+      <Media {...mediaProps} />
+    </Box>
+  )
+}
