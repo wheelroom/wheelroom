b5af12a2a59ebadf4416f5470e6b365762b7cab0
Jacco Meijer <jacco@jaccomeijer.nl>
2020-06-13 17:40:19 +0200

    chore: import order linter rule

--- packages/boilerplate/.eslintrc.js
+++ packages/boilerplate/.eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-    'plugin:react/recommended',
     'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
@@ -18,10 +20,13 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
     '@typescript-eslint/camelcase': 0,
     'react-hooks/rules-of-hooks': 2,
     'react-hooks/exhaustive-deps': 1,
+    'import/order': 2,
+    'import/no-unresolved': 0,
   },
   settings: {
     react: {
