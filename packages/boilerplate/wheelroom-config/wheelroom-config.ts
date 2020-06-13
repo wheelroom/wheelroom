da6f5826c78969c9b497ca611c4688ceab271803
Jacco Meijer <jacco@jaccomeijer.nl>
2020-06-13 21:09:52 +0200

    chore: use all split up models

--- packages/boilerplate/wheelroom-config/wheelroom-config.ts
+++ packages/boilerplate/wheelroom-config/wheelroom-config.ts
@@ -5,13 +5,15 @@ import { contentSets } from './content-sets/content-sets'
 import { replaceSets } from './replace-sets'
 
 import { coreModels } from './will-move-to-wr-module/models/core-models'
+import { tableModels } from './will-move-to-wr-module/models/table-models'
+import { blogModels } from './will-move-to-wr-module/models/blog-models'
 import { fieldDefautls } from './will-move-to-wr-module/field-defaults'
 import { commonFields } from './will-move-to-wr-module/common-fields'
 import { contentfulFieldDefinitions } from './will-move-to-wr-module/contentful-field-definitions'
 
 export const config: WheelroomConfig = {
   commonFields: commonFields,
-  components: coreModels,
+  components: { ...coreModels, ...blogModels, ...tableModels },
   fieldDefaults: fieldDefautls,
   plugins: [
     {
