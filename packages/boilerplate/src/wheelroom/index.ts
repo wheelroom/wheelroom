48a33a8b19ca4a62e1f052bc2b62c54d066eb50c
Jacco Meijer <jacco@jaccomeijer.nl>
2020-06-13 17:48:49 +0200

    chore: order exports

--- packages/boilerplate/src/wheelroom/index.ts
+++ packages/boilerplate/src/wheelroom/index.ts
@@ -1,54 +1,69 @@
-export { breakpointsPreset } from './theme/breakpoints-preset'
-export { classicGlobalReset } from './global-reset/classic-global-reset'
-export { htmlReset } from './global-reset/html-reset'
-export { mapsPreset } from './theme/maps-preset'
 export { NcssProps, Wheel } from './types/wheel'
-export { scalesPreset } from './theme/scales-preset'
-export { Seo } from './lib/seo'
-export { SeoProps } from './lib/seo-props'
-export { styledSystemConfig } from './theme/styled-system-config'
+
+// Theme
+export { breakpointsPreset } from './theme/breakpoints-preset'
 export { StyledSystemConfig } from './theme/types'
 export { StyledSystemTheme } from './theme/types'
-export { useEmbeds } from './models/embed/use-embed'
+export { scalesPreset } from './theme/scales-preset'
+export { styledSystemConfig } from './theme/styled-system-config'
+export { mapsPreset } from './theme/maps-preset'
 
-export { ActionProps } from './models/action/model-types'
-export { BlogProps } from './models/blog/model-types'
-export { BlogSectionProps } from './models/blog-section/model-types'
-export { EmbedProps } from './models/embed/model-types'
-export { GlobalsProps } from './models/globals/model-types'
-export { MediaBreakpointProps } from './models/media-breakpoint/model-types'
-export { NavigationProps } from './models/navigation/model-types'
-export { NavigationSectionProps } from './models/navigation-section/model-types'
-export { NavigationSegmentProps } from './models/navigation-segment/model-types'
-export { PageProps } from './models/page/model-types'
-export { PageSectionProps } from './models/page-section/model-types'
-export { TableProps } from './models/table/model-types'
-export { TableRowProps } from './models/table-row/model-types'
-export { TableSectionProps } from './models/table-section/model-types'
-export { TextProps } from './models/text/model-types'
-export { TextSectionProps } from './models/text-section/model-types'
-export { TopicProps } from './models/topic/model-types'
+// Glboal reset
+export { classicGlobalReset } from './global-reset/classic-global-reset'
+export { htmlReset } from './global-reset/html-reset'
 
-export { Action } from './models/action/action'
-export { BlogSection } from './models/blog-section/blog-section'
-export { Embed } from './models/embed/embed'
-export { MediaBreakpoint } from './models/media-breakpoint/media-breakpoint'
-export { NavigationSection } from './models/navigation-section/navigation-section'
-export { NavigationSegment } from './models/navigation-segment/navigation-segment'
-export { PageSection } from './models/page-section/page-section'
-export { Table } from './models/table/table'
-export { TableRow } from './models/table-row/table-row'
-export { TableSection } from './models/table-section/table-section'
-export { Text } from './models/text/text'
-export { TextSection } from './models/text-section/text-section'
-export { Topic } from './models/topic/topic'
+// Seo
+export { Seo } from './lib/seo'
+export { SeoProps } from './lib/seo-props'
 
-export type { BlogListWheelStyle } from './models/blog-section/list/blog-section-list-preset'
-export type { BlogWheelStyle } from './models/blog-section/blog/blog-section-preset'
+// Elements
 export type { ButtonElementStyle } from './elements/resets/button-reset'
 export type { ElementStyles } from './elements/types/element-styles'
+
+// Action
+export { Action } from './models/action/action'
+export { ActionProps } from './models/action/model-types'
+
+// Blog
+export { BlogProps } from './models/blog/model-types'
+export type { BlogWheelStyle } from './models/blog-section/blog/blog-section-preset'
+export type { BlogListWheelStyle } from './models/blog-section/list/blog-section-list-preset'
+
+// Blog section
+export { BlogSection } from './models/blog-section/blog-section'
+export { BlogSectionProps } from './models/blog-section/model-types'
+
+// Embed
+export { Embed } from './models/embed/embed'
+export { EmbedProps } from './models/embed/model-types'
+export { useEmbeds } from './models/embed/use-embed'
+
+// Globals
+export { GlobalsProps } from './models/globals/model-types'
+
+// Media breakpoint
+export { MediaBreakpoint } from './models/media-breakpoint/media-breakpoint'
+export { MediaBreakpointProps } from './models/media-breakpoint/model-types'
+
+// Navigation
+export { NavigationProps } from './models/navigation/model-types'
 export type { NavigationFooterWheelStyle } from './models/navigation-section/footer/presets/navigation-footer-preset'
 export type { NavigationHeaderWheelStyle } from './models/navigation-section/header/presets/navigation-header-preset'
+
+// Navigation section
+export { NavigationSection } from './models/navigation-section/navigation-section'
+export { NavigationSectionProps } from './models/navigation-section/model-types'
+
+// Navigation segment
+export { NavigationSegment } from './models/navigation-segment/navigation-segment'
+export { NavigationSegmentProps } from './models/navigation-segment/model-types'
+
+// Page
+export { PageProps } from './models/page/model-types'
+
+// Page section
+export { PageSection } from './models/page-section/page-section'
+export { PageSectionProps } from './models/page-section/model-types'
 export type { PageSectionBlockWheelStyle } from './models/page-section/presets/page-section-block-preset'
 export type { PageSectionCardWheelStyle } from './models/page-section/presets/page-section-card-preset'
 export type { PageSectionFeaturedWheelStyle } from './models/page-section/presets/page-section-featured-preset'
@@ -61,3 +76,27 @@ export type { PageSectionShowcaseWheelStyle } from './models/page-section/preset
 export type { PageSectionVideoWheelStyle } from './models/page-section/presets/page-section-video-preset'
 export type { TableSectionWheelStyle } from './models/table-section/price/table-section-preset'
 export type { TextSectionWheelStyle } from './models/text-section/text/text-section-preset'
+
+// Table
+export { Table } from './models/table/table'
+export { TableProps } from './models/table/model-types'
+
+// Table row
+export { TableRow } from './models/table-row/table-row'
+export { TableRowProps } from './models/table-row/model-types'
+
+// Table section
+export { TableSection } from './models/table-section/table-section'
+export { TableSectionProps } from './models/table-section/model-types'
+
+// Text
+export { Text } from './models/text/text'
+export { TextProps } from './models/text/model-types'
+
+// Text section
+export { TextSection } from './models/text-section/text-section'
+export { TextSectionProps } from './models/text-section/model-types'
+
+// Topic
+export { Topic } from './models/topic/topic'
+export { TopicProps } from './models/topic/model-types'
