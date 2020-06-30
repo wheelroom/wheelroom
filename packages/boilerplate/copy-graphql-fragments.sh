562ab03aaefedfe9b83440635eb2bbbfe95d2fb9
Jacco Meijer <jacco@jaccomeijer.nl>
2020-06-30 23:34:38 +0200

    feat: module checks in copy-graphql-fragments.sh

--- packages/boilerplate/copy-graphql-fragments.sh
+++ packages/boilerplate/copy-graphql-fragments.sh
@@ -4,30 +4,41 @@ echo "Cleaning up src/graphql-fragments"
 mkdir -p src/graphql-fragments
 rm -rf src/graphql-fragments/*
 
-echo "@wheelroom/wheel-blog"
-cp node_modules/@wheelroom/wheel-blog/src/models/blog/fragment.js src/graphql-fragments/blog-fragment.js
-cp node_modules/@wheelroom/wheel-blog/src/models/blog-section/fragment.js src/graphql-fragments/blog-section-fragment.js
+if [ -d "node_modules/@wheelroom/core" ]; then
+    echo "Copying fragments from @wheelroom/core"
+    cp node_modules/@wheelroom/core/src/models/action/fragment.js src/graphql-fragments/action-fragment.js
+    cp node_modules/@wheelroom/core/src/models/embed/fragment.js src/graphql-fragments/embed-fragment.js
+    cp node_modules/@wheelroom/core/src/models/globals/fragment.js src/graphql-fragments/globals-fragment.js
+    cp node_modules/@wheelroom/core/src/models/media-breakpoint/fragment.js src/graphql-fragments/media-breakpoint-fragment.js
+    cp node_modules/@wheelroom/core/src/models/page/fragment.js src/graphql-fragments/page-fragment.js
+fi
 
-echo "@wheelroom/core"
-cp node_modules/@wheelroom/core/src/models/action/fragment.js src/graphql-fragments/action-fragment.js
-cp node_modules/@wheelroom/core/src/models/embed/fragment.js src/graphql-fragments/embed-fragment.js
-cp node_modules/@wheelroom/core/src/models/globals/fragment.js src/graphql-fragments/globals-fragment.js
-cp node_modules/@wheelroom/core/src/models/media-breakpoint/fragment.js src/graphql-fragments/media-breakpoint-fragment.js
-cp node_modules/@wheelroom/core/src/models/page/fragment.js src/graphql-fragments/page-fragment.js
+if [ -d "node_modules/@wheelroom/wheel-blog" ]; then
+    echo "Copying fragments from @wheelroom/wheel-blog"
+    cp node_modules/@wheelroom/wheel-blog/src/models/blog/fragment.js src/graphql-fragments/blog-fragment.js
+    cp node_modules/@wheelroom/wheel-blog/src/models/blog-section/fragment.js src/graphql-fragments/blog-section-fragment.js
+fi
 
-echo "@wheelroom/wheel-navigation"
-cp node_modules/@wheelroom/wheel-navigation/src/models/navigation/fragment.js src/graphql-fragments/navigation-fragment.js
-cp node_modules/@wheelroom/wheel-navigation/src/models/navigation-section/fragment.js src/graphql-fragments/navigation-section-fragment.js
-cp node_modules/@wheelroom/wheel-navigation/src/models/navigation-segment/fragment.js src/graphql-fragments/navigation-segment-fragment.js
+if [ -d "node_modules/@wheelroom/wheel-navigation" ]; then
+    echo "Copying fragments from @wheelroom/wheel-navigation"
+    cp node_modules/@wheelroom/wheel-navigation/src/models/navigation/fragment.js src/graphql-fragments/navigation-fragment.js
+    cp node_modules/@wheelroom/wheel-navigation/src/models/navigation-section/fragment.js src/graphql-fragments/navigation-section-fragment.js
+    cp node_modules/@wheelroom/wheel-navigation/src/models/navigation-segment/fragment.js src/graphql-fragments/navigation-segment-fragment.js
+fi
 
-echo "@wheelroom/wheel-table"
-cp node_modules/@wheelroom/wheel-table/src/models/table-row/fragment.js src/graphql-fragments/table-row-fragment.js
-cp node_modules/@wheelroom/wheel-table/src/models/table-section/fragment.js src/graphql-fragments/table-section-fragment.js
+if [ -d "node_modules/@wheelroom/wheel-table" ]; then
+    echo "Copying fragments from @wheelroom/wheel-table"
+    cp node_modules/@wheelroom/wheel-table/src/models/table-row/fragment.js src/graphql-fragments/table-row-fragment.js
+    cp node_modules/@wheelroom/wheel-table/src/models/table-section/fragment.js src/graphql-fragments/table-section-fragment.js
+fi
 
-echo "@wheelroom/wheel-text"
-cp node_modules/@wheelroom/wheel-text/src/models/text-section/fragment.js src/graphql-fragments/text-section-fragment.js
+if [ -d "node_modules/@wheelroom/wheel-text" ]; then
+    echo "Copying fragments from @wheelroom/wheel-text"
+    cp node_modules/@wheelroom/wheel-text/src/models/text-section/fragment.js src/graphql-fragments/text-section-fragment.js
+fi
 
-echo "@wheelroom/topic-wheel"
-cp node_modules/@wheelroom/wheel-topic/src/models/topic/fragment.js src/graphql-fragments/topic-fragment.js
-cp node_modules/@wheelroom/wheel-topic/src/models/topic-section/fragment.js src/graphql-fragments/topic-section-fragment.js
-
+if [ -d "node_modules/@wheelroom/wheel-topic" ]; then
+    echo "Copying fragments from @wheelroom/wheel-topic"
+    cp node_modules/@wheelroom/wheel-topic/src/models/topic/fragment.js src/graphql-fragments/topic-fragment.js
+    cp node_modules/@wheelroom/wheel-topic/src/models/topic-section/fragment.js src/graphql-fragments/topic-section-fragment.js
+fi
