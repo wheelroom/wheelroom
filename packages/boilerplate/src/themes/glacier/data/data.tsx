98c1c8605407e3e7b057b4c5c18325f8f97f4d8d
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-08-26 13:33:28 +0200

    chore: add doc data model in Glacier theme

--- packages/boilerplate/src/themes/glacier/data/data.tsx
+++ packages/boilerplate/src/themes/glacier/data/data.tsx
@@ -1,6 +1,7 @@
 import { SectionWheelThemeData } from '@wheelroom/core'
 import { blogSectionData } from './blog-data'
 import { customSectionData } from './custom-data'
+import { docSectionData } from './doc-data'
 import { navigationSectionData } from './navigation-data'
 import { tableSectionData } from './table-data'
 import { textSectionData } from './text-data'
@@ -9,6 +10,7 @@ import { topicSectionData } from './topic-data'
 export const data: SectionWheelThemeData = {
   blogSection: blogSectionData,
   customSection: customSectionData,
+  docSection: docSectionData,
   navigationSection: navigationSectionData,
   tableSection: tableSectionData,
   textSection: textSectionData,
