d23bb13fb5f42ddf50d1304fd19dca6126625fd8
Jacco Meijer <jacco@jaccomeijer.nl>
2021-03-17 15:42:06 +0100

    wip: render index

--- packages/any/src/generate-index/intrinsic-elements.ts
+++ packages/any/src/generate-index/intrinsic-elements.ts
@@ -182,16 +182,3 @@ const intrinsicElements = [
   'use',
   'view',
 ]
-
-const RenderAnyComponents = (elements) => {
-  return elements.map((element) => {
-    const jsxComponentName = element[0].toUpperCase() + element.slice(1)
-    return console.log(
-      `export const ${jsxComponentName}: React.FC<AnyProps['${element}']> = (props) => (
-  <Any is="${element}" {...props} />
-)`
-    )
-  })
-}
-
-RenderAnyComponents(intrinsicElements)
