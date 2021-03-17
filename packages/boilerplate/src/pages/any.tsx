aeb4b919327a178f60c0e7d3317fe54ae815b2ad
Jacco Meijer <jacco@jaccomeijer.nl>
2021-03-17 18:38:18 +0100

    wip: any component

---
+++ packages/boilerplate/src/pages/any.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Any, AnyComponent } from '@wheelroom/any'
+
+const AnyPage = () => (
+  <div>
+    Hey
+    <Any />
+    <AnyComponent />
+  </div>
+)
+
+export default AnyPage
