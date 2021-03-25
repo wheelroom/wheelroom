e2f3368a3a8b29a4dbe0dcecba8192ec3eda97bb
Jacco Meijer <jacco@jaccomeijer.nl>
2021-03-25 23:04:40 +0100

    chore: remove resets.js

--- apps/gatsby-starter/src/pages/index.tsx
+++ apps/gatsby-starter/src/pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { PageProps } from 'gatsby'
 import { Ul, Li } from '@wheelroom/any/elements'
+import { globalReset } from '@wheelroom/any/resets/global-reset'
 
 const Home = (props: PageProps) => {
+  console.log('global resets', globalReset)
   return (
     <Ul css={{ backgroundColor: 'lightblue' }}>
       <Li css={{ color: 'red' }}>Hello from a typescript world!</Li>
--- packages/any/src/resets.ts
+++
@@ -1,3 +0,0 @@
-export { anyReset } from './resets/any-reset'
-export { globalReset } from './resets/global-reset'
-export { elementResetMap } from './resets/element-reset-map'
