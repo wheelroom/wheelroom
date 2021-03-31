3987ee449eee6da8ae56217b9ca8533425ab4025
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-03-31 17:04:53 +0200

    chore: add console log

--- starters/gatsby-starter/src/pages/index.tsx
+++ starters/gatsby-starter/src/pages/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import { PageProps } from 'gatsby'
 import { Ul, Li } from '@wheelroom/any/elements'
 import { globalReset } from '@wheelroom/any/resets/global-reset'
 
 const Home = (props: PageProps) => {
   console.log('global resets', globalReset)
+  console.log('props', props)
   return (
     <Ul css={{ backgroundColor: 'lightblue' }}>
       <Li css={{ color: 'red' }}>Hello from a typescript world!</Li>
