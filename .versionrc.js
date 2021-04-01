93d331283f667d01c1e9b02bf8f6d637a58abbcf
Jacco Meijer <jacco@jaccomeijer.nl>
2021-04-01 20:35:51 +0200

    chore: cleanup

--- .versionrc.js
+++ .versionrc.js
@@ -1,14 +1,21 @@
+const updater = require('./version-updater')
+
 const rootPackage = {
-    filename: 'package.json',
-    updater: require('./version-updater'),
-  }
-  
+  filename: './package.json',
+  updater,
+}
+
 const anyPackage = {
-  filename: 'packages/any/package.json',
-  updater: require('./version-updater'),
+  filename: './packages/any/package.json',
+  updater,
+}
+
+const gatsbyStarterPackage = {
+  filename: './starters/gatsby-starter/package.json',
+  updater,
 }
 
 module.exports = {
-  bumpFiles: [anyPackage],
+  bumpFiles: [anyPackage, rootPackage, gatsbyStarterPackage],
   packageFiles: [rootPackage],
 }
