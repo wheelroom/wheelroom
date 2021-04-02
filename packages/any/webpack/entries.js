6d3505f291c67f85bb3822b382070e0bb39abcd6
Jacco Meijer <jacco@jaccomeijer.nl>
2021-04-02 16:55:55 +0200

    chore: setup webpack browser bundles

--- packages/any/webpack/entries.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  { name: 'Any', extension: 'tsx' },
-  { name: 'elements', extension: 'tsx' },
-  { name: 'resets/any-reset', extension: 'ts' },
-  { name: 'resets/element-reset-map', extension: 'ts' },
-  { name: 'resets/global-reset', extension: 'ts' },
-]
--- packages/any/webpack/webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const entries = require('./entries')
-const webpackEntryFactory = require('./webpack-entry-factory')
-const webpackProdFactory = require('./webpack-prod-factory')
-
-module.exports = entries.map((entry) => ({
-  ...webpackProdFactory({ entry }),
-  ...webpackEntryFactory({ entry }),
-}))
