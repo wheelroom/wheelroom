f803dfc8047286e45dc4ce2fc4b3481213f9b483
Jacco Meijer <jacco@jaccomeijer.nl>
2021-04-02 13:29:12 +0200

    chore: add types

--- starters/gatsby-starter/.versionrc.cjs
+++ starters/gatsby-starter/.versionrc.cjs
@@ -12,3 +12,14 @@ const gatsbyStarterPackage = {
 
 export const bumpFiles = [rootPackage, gatsbyStarterPackage]
 export const packageFiles = [rootPackage]
+
+export const types = [
+  { type: 'feat', section: 'Features' },
+  { type: 'fix', section: 'Bug Fixes' },
+  { type: 'chore', section: 'Commits' },
+  { type: 'docs', section: 'Documentation' },
+  { type: 'style', section: 'Styling' },
+  { type: 'refactor', section: 'Code Refactoring' },
+  { type: 'perf', hidden: true },
+  { type: 'test', hidden: true },
+]
