d8e025fd49b96c3bcd4c25e62818000de75a0b3f
Jacco Meijer <jacco@jaccomeijer.nl>
2021-04-13 22:57:13 +0200

    feat: link command for creating linked node_modules outside monorepo

--- packages/make/src/link-command.ts
+++ packages/make/src/link-command.ts
@@ -3,9 +3,10 @@
  * - Create symlinks in node_modules to all package/build folders
  */
 
+import { mkdir, symlink } from 'fs/promises'
+import { existsSync } from 'fs'
 import Arborist from '@npmcli/arborist'
 import { ArboristNode } from './npm'
-// import { mkdir } from 'fs/promises'
 
 export interface LinkCommand {
   monoRepoPath: string
@@ -15,19 +16,26 @@ export const linkCommand = async ({ monoRepoPath }: LinkCommand) => {
   const cloneDir = 'build'
   const arborist = new Arborist({ path: monoRepoPath })
   const rootNode = await arborist.loadActual()
-  const fsChildren = rootNode.fsChildren
-  if (fsChildren.length === 0) {
+  const fsChildrenArray = Array.from(rootNode.fsChildren) as ArboristNode[]
+  if (fsChildrenArray.length === 0) {
     console.log(`No packages found in ${monoRepoPath}`)
     process.exit(0)
   }
-  fsChildren.forEach((node: ArboristNode) => {
+  for (const node of fsChildrenArray) {
     const splitPath = node.package.name.split('/')
     const linkName = splitPath.pop()
     const path = ['node_modules', ...splitPath].join('/')
     const linkFrom = ['node_modules', ...splitPath, linkName].join('/')
     const linkTo = `${node.path}/${cloneDir}`
-    // await mkdir(`node_modules`, { recursive: true })
-    console.log(`mkdir -p ${path}`)
-    console.log(`ln -s ${linkFrom} => ${linkTo}`)
-  })
+    const linkFromExists = existsSync(linkFrom)
+    const linkToExists = existsSync(linkTo)
+    if (linkFromExists) console.log(`Skipping: ${linkFrom} already exists`)
+    if (!linkFromExists && !linkToExists)
+      console.log(`Skipping: ${linkTo} does not exist`)
+    if (!linkFromExists && linkToExists) {
+      await mkdir(path, { recursive: true })
+      await symlink(linkTo, linkFrom)
+      console.log(`Created: ${linkFrom} -> <pkg>/${cloneDir}`)
+    }
+  }
 }
