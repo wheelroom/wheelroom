71f4a6955c9d4ebb5d326570ce0366e5a1fc13b8
Jacco Meijer <jacco@jaccomeijer.nl>
2021-04-14 17:52:58 +0200

    chore: implement publish

--- packages/make/src/commands/publish.ts
+++ packages/make/src/commands/publish.ts
@@ -1,41 +1,42 @@
 import path from 'path'
 import { MakeContext } from '../get-make-context'
-// import { cmdRun } from '../npm'
+import { cmdRun } from '../npm'
 
 export interface PublishMakeContext {
   makeContext: MakeContext
 }
 
 export const publish = async ({ makeContext }: PublishMakeContext) => {
-  // const { buildNodes, cloneDir } = makeContext
-  // for (const publishNode of buildNodes) {
-  //   if (!publishNode.package.private) {
-  //     await cmdRun({
-  //       cmd: 'npm',
-  //       args: ['publish'],
-  //       cloneDir,
-  //       node: publishNode,
-  //     })
-  //   }
-  // }
-  const { buildNodes, rootNode } = makeContext
+  const { buildNodes, rootNode, cloneDir } = makeContext
+  for (const publishNode of buildNodes) {
+    if (!publishNode.package.private) {
+      await cmdRun({
+        cmd: 'npm',
+        args: ['publish'],
+        cloneDir,
+        node: publishNode,
+      })
+    }
+  }
   const gitAddFiles = []
   for (const publishNode of buildNodes) {
     gitAddFiles.push(path.relative(process.cwd(), publishNode.path))
   }
+  const version = `v${rootNode.package.version}`
 
-  const tagMessage = `# v${rootNode.package.version}\n\n${makeContext.newChangeLog}`
+  // const tagMessage = `# ${version}\n\n${makeContext.newChangeLog}`
   const cmd = 'git'
+
   let args
   args = ['add', ...gitAddFiles]
-  console.log(cmd, args.join(' '))
+  cmdRun({ cmd, args, node: rootNode })
 
-  args = ['commit', '-m', `v${rootNode.package.version}`, ...gitAddFiles]
-  console.log(cmd, args.join(' '))
+  args = ['commit', '-m', version, ...gitAddFiles]
+  cmdRun({ cmd, args, node: rootNode })
 
-  args = ['tag', '-m', tagMessage]
-  console.log(cmd, args.join(' '))
+  args = ['tag', '-m', version, version]
+  cmdRun({ cmd, args, node: rootNode })
 
   args = ['push', '--follow-tags']
-  console.log(cmd, args.join(' '))
+  cmdRun({ cmd, args, node: rootNode })
 }
