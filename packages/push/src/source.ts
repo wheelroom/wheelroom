2f8853a931fc9c1fccc24f597856f361cd335762
Jacco Meijer <jacco@jaccomeijer.nl>
2021-04-22 19:39:53 +0200

    chore: add test types

--- packages/push/src/source.ts
+++ packages/push/src/source.ts
@@ -1,7 +1,13 @@
+export type Variant = 'external 1' | 'external 2' | 'external 3'
+
 /** @platform contentful */
 export interface Topic {
   /** {shortString} Topic heading */
   heading: string
   /** {longString} Topic abstract */
   abstract: string
+  /** {arrayType} Topic variant */
+  variant: Variant
+  /** {anotherType} Topic variant */
+  otherVariant: 'variant 1' | 'variant 2' | 'variant 3'
 }
