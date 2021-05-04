45fd5b045336907e97c3d009abc6d0f31a17f50a
Jacco Meijer <jacco@jaccomeijer.nl>
2021-05-04 16:47:37 +0200

    chore: implement githubRelease method

--- packages/make/src/lib/github-release.ts
+++ packages/make/src/lib/github-release.ts
@@ -1,53 +1,31 @@
-/**
- * Simple Github release demo
- *
- * Start dev mode like this:
- *
- * npm run dev
- *
- * Then:
- *
- * GITHUB_TOKEN=ghp_xxx node build/lib/github-release.js
- *
- * Docs are here: https://octokit.github.io/rest.js/v18#repos-list-releases
- *
- */
-
+import { readFileSync } from 'fs'
 import { Octokit } from '@octokit/rest'
+import { ArboristNode } from './arborist'
 
-const main = async () => {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
-
-  // CREATE A RELEASE
-  // const release = await octokit.rest.repos.createRelease({
-  //   body: '# Heading\n\n- list 1\n- list 2\n\n## Other heading\n',
-  //   draft: false,
-  //   name: '6.19.1',
-  //   owner: 'wheelroom',
-  //   repo: 'wheelroom',
-  //   tag_name: '6.19.1',
-  // })
-  // console.log('release', release)
-
-  // DELETE A RELEASE
-  const releaseId = 42375489
-  await octokit.rest.repos.deleteRelease({
-    owner: 'wheelroom',
-    repo: 'wheelroom',
-    release_id: releaseId,
-  })
-  console.log('Deleted release', 42375489)
-
-  // LIST RELEASES
-  // const releases = await octokit.rest.repos.listReleases({
-  //   owner: 'wheelroom',
-  //   repo: 'wheelroom',
-  // })
-  // const releaseMap = releases.data.map((release) => [
-  //   release.tag_name,
-  //   release.id,
-  // ])
-  // console.log('releaseMap', releaseMap)
+interface GithubRelease {
+  packageName: string
+  rootNode: ArboristNode
+  tag: string
+  token: string
 }
 
-main()
+export const githubRelease = async ({
+  packageName,
+  rootNode,
+  tag,
+  token,
+}: GithubRelease) => {
+  const octokit = new Octokit({ auth: token })
+  const releaseFile = `${rootNode.path}/RELEASE.md`
+  const body = readFileSync(releaseFile, 'utf-8')
+  const [owner, repo] = packageName.split('/')
+
+  await octokit.rest.repos.createRelease({
+    body,
+    draft: false,
+    name: `Release ${tag}`,
+    owner: owner.slice(1),
+    repo,
+    tag_name: tag,
+  })
+}
