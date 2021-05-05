9d68c5607268f254ae46dd31ed83c82d547f06a9
Jacco Meijer <jacco@jaccomeijer.nl>
2021-05-05 12:46:30 +0200

    chore: cleanup external module config

--- packages/nextjs-starter/next.config.js
+++ packages/nextjs-starter/next.config.js
@@ -2,12 +2,4 @@ module.exports = {
   future: {
     webpack5: true,
   },
-  // Compile external modules
-  webpack: (config, { defaultLoaders }) => {
-    config.module.rules.push({
-      test: /\.+(tsx|ts)?$/,
-      use: [defaultLoaders.babel],
-    })
-    return config
-  },
 }
