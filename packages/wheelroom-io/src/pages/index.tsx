16f7ed8755a798b62c65df940a1520946966309f
Jacco Meijer <jacco@jaccomeijer.nl>
2021-05-18 16:36:11 +0200

    chore: update title

--- packages/wheelroom-io/src/pages/index.tsx
+++ packages/wheelroom-io/src/pages/index.tsx
@@ -19,7 +19,7 @@ const HomePage = (props: PageProps) => {
     <>
       <Global styles={globalReset} />
       <Div css={objectStyles}>
-        Gatsby + TypeScript + Emotion + Normalize.css
+        Gatsby + TypeScript + Any (emotion/normalize.css)
       </Div>
       <GraphqlBuildTime />
     </>
