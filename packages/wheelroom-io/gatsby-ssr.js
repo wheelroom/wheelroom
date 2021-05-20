34c76938c0583a511c4ce71503fdfd999c64ae63
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-05-20 22:14:25 +0200

    chore: fix src file path

--- packages/wheelroom-io/gatsby-ssr.js
+++ packages/wheelroom-io/gatsby-ssr.js
@@ -1,5 +1,5 @@
 export const onRenderBody = ({ setPreBodyComponents, setHeadComponents }) => {
   setPreBodyComponents([
-    <script key="1" type="text/javascript" src={'set-css-vars.js'} />,
+    <script key="1" type="text/javascript" src={'/set-css-vars.js'} />,
   ])
 }
