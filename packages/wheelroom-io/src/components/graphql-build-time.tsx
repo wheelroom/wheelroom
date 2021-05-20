9e54831312e6d3ef28d52bc60b5200fc005cbaac
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-05-21 00:24:40 +0200

    chore: use Any components

--- packages/wheelroom-io/src/components/graphql-build-time.tsx
+++ packages/wheelroom-io/src/components/graphql-build-time.tsx
@@ -1,4 +1,26 @@
-import { graphql, useStaticQuery } from 'gatsby'
+import { Li, Ul } from '@wheelroom/any/elements'
+import { graphql, Link, useStaticQuery } from 'gatsby'
+import { fontStyle } from '../pages/index'
+
+interface BlogCollectionQuery {
+  slug: string
+  title: string
+}
+
+const GraphqlBuildTime = () => {
+  const data = useStaticQuery(dataId)
+  return (
+    <Ul>
+      {data.contentful.blogCollection.items.map((blog: BlogCollectionQuery) => (
+        <Li key={blog.slug} css={fontStyle}>
+          <Link to={`/blog/${blog.slug}`}>{blog.title}</Link>
+        </Li>
+      ))}
+    </Ul>
+  )
+}
+
+export default GraphqlBuildTime
 
 const dataId = graphql`
   query {
@@ -12,18 +34,3 @@ const dataId = graphql`
     }
   }
 `
-
-const GraphqlBuildTime = () => {
-  const data = useStaticQuery(dataId)
-  return (
-    <>
-      {data.contentful.blogCollection.items.map((blog: any) => (
-        <p key={blog.slug}>
-          <a href={`/blog/${blog.slug}`}>{blog.title}</a>
-        </p>
-      ))}
-    </>
-  )
-}
-
-export default GraphqlBuildTime
