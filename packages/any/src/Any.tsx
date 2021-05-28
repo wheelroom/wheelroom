eb353102c3333accd18f510f975dfea751e6479c
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-05-28 15:38:41 +0200

    chore: use jsx types from @emotion/react

--- packages/any/src/Any.tsx
+++ packages/any/src/Any.tsx
@@ -1,23 +1,19 @@
-import { CSSObject, Interpolation, jsx, Theme } from '@emotion/react'
+import { CSSObject, jsx } from '@emotion/react'
 import { anyReset } from './resets/any-reset'
 import { elementResetMap, ElementResetName } from './resets/element-reset-map'
 
-type JSXIntrElName = keyof JSX.IntrinsicElements
+type JSXIntrElName = keyof jsx.JSX.IntrinsicElements
 
 /**
  * Extend JSX.IntrinsicElements with AnyProps: `is` and `css`
  */
 export type AnyProps = {
-  [K in JSXIntrElName]: JSX.IntrinsicElements[K] & {
+  [K in JSXIntrElName]: jsx.JSX.IntrinsicElements[K] & {
     /**
      * Specify that a standard HTML element should behave like a defined custom built-in element
      * @see https://html.spec.whatwg.org/multipage/custom-elements.html#attr-is
      */
     is?: JSXIntrElName
-    /**
-     * CSS interpolation from @emotion/react
-     */
-    css?: Interpolation<Theme>
   }
 }
 
