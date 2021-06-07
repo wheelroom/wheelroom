15d8ae80351c080a59169583bd197e163f13106e
Jacco Meijer <jacco@jaccomeijer.nl>
2021-06-07 23:21:31 +0200

    chore: update content variantMap

--- packages/wheelroom-io/src/components/topic/topic-content/topic-content-variants.tsx
+++ packages/wheelroom-io/src/components/topic/topic-content/topic-content-variants.tsx
@@ -1,6 +1,12 @@
+import { TopicActionsVariantMap } from './topic-actions/topic-actions-variants'
+import { TopicTextVariantMap } from './topic-text/topic-text-variants'
+
 export const topicContentVariants = ['myVar'] as const
 
 export type TopicContentVariant = typeof topicContentVariants[number]
+
 export type TopicContentVariantMap = Partial<
   Record<'topicContent', TopicContentVariant>
->
+> &
+  TopicTextVariantMap &
+  TopicActionsVariantMap
