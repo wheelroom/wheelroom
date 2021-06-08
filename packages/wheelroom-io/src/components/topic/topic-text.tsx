3259d786f474b8080bb5427229a37535002922fe
Jacco Meijer <jacco@jaccomeijer.nl>
2021-06-08 21:19:10 +0200

    chore: dynamic heading

--- packages/wheelroom-io/src/components/topic/topic-text.tsx
+++ packages/wheelroom-io/src/components/topic/topic-text.tsx
@@ -1,6 +1,6 @@
 import { Div } from '@wheelroom/any/elements'
 import { Abstract } from '../typography/abstract'
-import { H3 } from '../typography/heading'
+import { Heading } from '../typography/heading'
 import { TopicOptions } from './topic-options'
 import { TopicVariantMap } from './topic-variants'
 import { topicTextVariantStyle } from './topic-text-variant-style'
@@ -19,7 +19,7 @@ export interface TopicTextProps {
 export const TopicText = (props: TopicTextProps) => {
   return (
     <Div css={topicTextVariantStyle({ variant: props.variantMap.topicText })}>
-      <H3>{props.model.heading}</H3>
+      <Heading is="h3">{props.model.heading}</Heading>
       <Abstract>{props.model.abstract}</Abstract>
     </Div>
   )
--- packages/wheelroom-io/src/components/typography/heading.tsx
+++ packages/wheelroom-io/src/components/typography/heading.tsx
@@ -1,12 +1,4 @@
-import { AnyProps } from '@wheelroom/any/Any'
-import {
-  H1 as AnyH1,
-  H2 as AnyH2,
-  H3 as AnyH3,
-  H4 as AnyH4,
-  H5 as AnyH5,
-  H6 as AnyH6,
-} from '@wheelroom/any/elements'
+import { Any, AnyProps } from '@wheelroom/any/Any'
 import { mediaQuery } from '../../lib/media-query'
 
 const headingStyle = {
@@ -14,66 +6,35 @@ const headingStyle = {
   marginTop: 0,
 }
 
-export const H1 = (props: AnyProps['h1']) => (
-  <AnyH1
-    css={mediaQuery({
-      ...headingStyle,
-      fontSize: ['42px', '42px', '56px', '56px'],
-    })}
-  >
+const styleMap = {
+  h1: {
+    ...headingStyle,
+    fontSize: ['42px', '42px', '56px', '56px'],
+  },
+  h2: {
+    ...headingStyle,
+    fontSize: ['32px', '32px', '42px', '42px'],
+  },
+  h3: {
+    ...headingStyle,
+    fontSize: ['20px', '20px', '24px', '24px'],
+  },
+  h4: {
+    ...headingStyle,
+    fontSize: ['18px', '18px', '20px', '20px'],
+  },
+  h5: {
+    ...headingStyle,
+    fontSize: ['18px', '18px', '20px', '20px'],
+  },
+  h6: {
+    ...headingStyle,
+    fontSize: ['18px', '18px', '20px', '20px'],
+  },
+}
+type HeadingName = keyof typeof styleMap
+export const Heading = (props: AnyProps['h1']) => (
+  <Any is={props.is} css={mediaQuery(styleMap[props.is as HeadingName])}>
     {props.children}
-  </AnyH1>
-)
-export const H2 = (props: AnyProps['h2']) => (
-  <AnyH2
-    css={mediaQuery({
-      ...headingStyle,
-      fontSize: ['32px', '32px', '42px', '42px'],
-    })}
-  >
-    {props.children}
-  </AnyH2>
-)
-export const H3 = (props: AnyProps['h3']) => (
-  <AnyH3
-    css={mediaQuery({
-      ...headingStyle,
-      fontSize: ['20px', '20px', '24px', '24px'],
-    })}
-  >
-    {props.children}
-  </AnyH3>
-)
-
-export const H4 = (props: AnyProps['h4']) => (
-  <AnyH4
-    css={mediaQuery({
-      ...headingStyle,
-      fontSize: ['18px', '18px', '20px', '20px'],
-    })}
-  >
-    {props.children}
-  </AnyH4>
-)
-
-export const H5 = (props: AnyProps['h5']) => (
-  <AnyH5
-    css={mediaQuery({
-      ...headingStyle,
-      fontSize: ['18px', '18px', '20px', '20px'],
-    })}
-  >
-    {props.children}
-  </AnyH5>
-)
-
-export const H6 = (props: AnyProps['h6']) => (
-  <AnyH6
-    css={mediaQuery({
-      ...headingStyle,
-      fontSize: ['18px', '18px', '20px', '20px'],
-    })}
-  >
-    {props.children}
-  </AnyH6>
+  </Any>
 )
