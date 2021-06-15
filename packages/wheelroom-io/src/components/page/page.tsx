0f8bb94bf78ef46545f9530e5a8d8dc4ae0b0025
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-06-15 19:13:59 +0200

    chore: add fontStyle to Global styles

--- packages/wheelroom-io/src/components/page/page.tsx
+++ packages/wheelroom-io/src/components/page/page.tsx
@@ -1,5 +1,4 @@
-import { Div } from '@wheelroom/any/elements'
-import { CSSObject, Global } from '@emotion/react'
+import { Global } from '@emotion/react'
 import { globalReset } from '@wheelroom/any/resets'
 import { graphql, PageProps } from 'gatsby'
 import { GlobalsProvider } from '../../lib/globals-provider'
@@ -7,15 +6,13 @@ import { ContentfulGlobals } from '../globals/contentful-globals'
 import { Sections } from './sections'
 import { ContentfulPage } from './contentful-page'
 
-// export type Page = {
-//   page?: ContentfulPage
-// }
-
-export const fontStyle: CSSObject = {
-  fontFamily: `-apple-system, BlinkMacSystemFont,
-  “Segoe UI”, “Roboto”, “Oxygen”,
-  “Ubuntu”, “Cantarell”, “Fira Sans”,
-  “Droid Sans”, “Helvetica Neue”, sans-serif`,
+export const fontStyle = {
+  body: {
+    fontFamily: `-apple-system, BlinkMacSystemFont,
+    “Segoe UI”, “Roboto”, “Oxygen”,
+    “Ubuntu”, “Cantarell”, “Fira Sans”,
+    “Droid Sans”, “Helvetica Neue”, sans-serif`,
+  },
 }
 
 export type PageQuery = {
@@ -38,10 +35,8 @@ const Page = (props: PageProps<PageQuery, PageContext>) => {
   console.log('pageId', props.pageContext.pageId)
   return (
     <GlobalsProvider value={{ globals, site }}>
-      <Global styles={globalReset} />
-      <Div css={fontStyle}>
-        <Sections model={{ sections: page.sectionsCollection.items }} />
-      </Div>
+      <Global styles={[globalReset, fontStyle]} />
+      <Sections model={{ sections: page.sectionsCollection.items }} />
     </GlobalsProvider>
   )
 }
