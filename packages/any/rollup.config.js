d6f143c7fdb09b21f3ec9584d72876ed696c2a5f
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-06-16 15:12:06 +0200

    refactor: change any package exports to react

--- packages/any/rollup.config.js
+++ packages/any/rollup.config.js
@@ -6,7 +6,6 @@
 import pluginTypescript from '@rollup/plugin-typescript'
 import pluginCommonjs from '@rollup/plugin-commonjs'
 import pluginNodeResolve from '@rollup/plugin-node-resolve'
-import multi from '@rollup/plugin-multi-entry'
 import json from '@rollup/plugin-json'
 import pkg from './package.json'
 import rootPkg from '../../package.json'
@@ -20,10 +19,6 @@ const globals = {
   'react/jsx-runtime': 'jsx',
 }
 const external = Object.keys(globals)
-
-const anyInputFiles = ['any.tsx']
-const elementsInputFiles = ['elements.tsx']
-const resetsInputFiles = ['resets.ts']
 
 const banner = `
   /**
@@ -45,21 +40,18 @@ const plugins = [
   }),
   // so Rollup can convert TypeScript to JavaScript
   pluginTypescript({ tsconfig: 'tsconfig.packages.json' }),
-  // Using multiple input files as entry points will yield a bundle with
-  // exports for each
-  multi(),
   json(),
 ]
 
 export default [
   {
     external,
-    input: anyInputFiles.map((file) => `./src/${file}`),
+    input: './src/react.ts',
     output: [
       {
         banner,
         exports: 'named',
-        file: `./build/any.mjs`,
+        file: `./build/react.mjs`,
         format: 'es',
         globals,
         sourcemap: false,
@@ -67,53 +59,7 @@ export default [
       {
         banner,
         exports: 'named',
-        file: `./build/any.cjs`,
-        format: 'cjs',
-        globals,
-        sourcemap: false,
-      },
-    ],
-    plugins,
-  },
-  {
-    external,
-    input: elementsInputFiles.map((file) => `./src/${file}`),
-    output: [
-      {
-        banner,
-        exports: 'named',
-        file: `./build/elements.mjs`,
-        format: 'es',
-        globals,
-        sourcemap: false,
-      },
-      {
-        banner,
-        exports: 'named',
-        file: `./build/elements.cjs`,
-        format: 'cjs',
-        globals,
-        sourcemap: false,
-      },
-    ],
-    plugins,
-  },
-  {
-    external,
-    input: resetsInputFiles.map((file) => `./src/${file}`),
-    output: [
-      {
-        banner,
-        exports: 'named',
-        file: `./build/resets.mjs`,
-        format: 'es',
-        globals,
-        sourcemap: false,
-      },
-      {
-        banner,
-        exports: 'named',
-        file: `./build/resets.cjs`,
+        file: `./build/react.cjs`,
         format: 'cjs',
         globals,
         sourcemap: false,
--- packages/any/src/react.ts
+++ packages/any/src/react.ts
@@ -1,3 +1,5 @@
+export * from './any'
+export * from './elements'
 export * from './resets/any-reset'
 export * from './resets/element-reset-map'
 export * from './resets/global-reset'
