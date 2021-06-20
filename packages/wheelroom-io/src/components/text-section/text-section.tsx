33f1d522882e2a2a9f6403967323dc93815cffd0
Jacco Meijer <jacco@jaccomeijer.nl>
2021-06-20 11:40:07 +0200

    chore: add missing block renders

--- packages/wheelroom-io/src/components/text-section/text-section.tsx
+++ packages/wheelroom-io/src/components/text-section/text-section.tsx
@@ -1,6 +1,21 @@
 /* eslint-disable react/display-name */
 import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types'
-import { AnyProps, Div, B, U, I, Code, A } from '@wheelroom/any/react'
+import {
+  A,
+  AnyProps,
+  B,
+  Code,
+  Div,
+  Hr,
+  I,
+  Li,
+  Ol,
+  U,
+  Ul,
+  Blockquote,
+  Tr,
+  Td,
+} from '@wheelroom/any/react'
 import {
   documentToReactComponents,
   Options,
@@ -52,7 +67,15 @@ export const TextSection = ({ model, ...props }: TextSectionProps) => {
       [BLOCKS.HEADING_4]: (node, children) => <H4>{children}</H4>,
       [BLOCKS.HEADING_5]: (node, children) => <H5>{children}</H5>,
       [BLOCKS.HEADING_6]: (node, children) => <H6>{children}</H6>,
+      [BLOCKS.HR]: () => <Hr />,
+      [BLOCKS.LIST_ITEM]: (node, children) => <Li>{children}</Li>,
+      [BLOCKS.OL_LIST]: (node, children) => <Ol>{children}</Ol>,
       [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+      [BLOCKS.QUOTE]: (node, children) => <Blockquote>{children}</Blockquote>,
+      [BLOCKS.TABLE_CELL]: (node, children) => <Td>{children}</Td>,
+      [BLOCKS.TABLE_ROW]: (node, children) => <Tr>{children}</Tr>,
+      [BLOCKS.TABLE]: (node, children) => <Blockquote>{children}</Blockquote>,
+      [BLOCKS.UL_LIST]: (node, children) => <Ul>{children}</Ul>,
       [BLOCKS.EMBEDDED_ENTRY]: (node) => {
         const entry = links?.entries.block.find(
           (entry) => entry.sys?.id === node.data.target.sys.id
