454ce92acd0c6b9b7e57ae208bfeca6b62798485
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-06-21 17:42:51 +0200

    chore: refactor Anchor element

--- packages/wheelroom-io/src/components/elements/anchor.tsx
+++ packages/wheelroom-io/src/components/elements/anchor.tsx
@@ -1,23 +1,45 @@
 import { AnyProps, A as AnyA } from '@wheelroom/any/react'
+import { mediaQuery } from '../../lib/media-query'
 
 export type AnchorVariant = 'link' | 'header' | 'footer' | 'sitemap'
+export type AnchorOption = ''
+export type AnchorOptions = Partial<Record<AnchorOption, boolean>>
 
 type AnyAProps = AnyProps['a']
-export type AnchorProps = AnyAProps
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const anchorStyleFactory = (args: { variant?: any; options?: any }) => {
-  return {
-    fontSize: 18,
-    backgroundColor: 'transparent',
-    color: 'var(--colors-azure)',
-    border: 0,
-    padding: 0,
-    textDecoration: 'underline',
-  }
+export interface AnchorProps extends AnyAProps {
+  options?: AnchorOptions
+  variant?: AnchorVariant
 }
 
-export const Anchor = (props: AnchorProps) => {
-  const css = anchorStyleFactory({})
+const baseStyle = {
+  fontSize: 18,
+  backgroundColor: 'transparent',
+  color: 'var(--colors-azure)',
+  border: 0,
+  padding: 0,
+  textDecoration: 'underline',
+}
+
+const styleMap: Partial<Record<AnchorVariant, any>> = {
+  link: baseStyle,
+  header: baseStyle,
+  footer: baseStyle,
+  sitemap: baseStyle,
+}
+
+export const anchorStyleFactory = (args: {
+  variant?: AnchorVariant
+  options?: AnchorOptions
+}) => {
+  const useVariant = args.variant || 'link'
+  const baseStyle = styleMap[useVariant]
+  return mediaQuery([baseStyle])
+}
+
+export const Anchor = ({ variant, options, ...props }: AnchorProps) => {
+  const css = anchorStyleFactory({
+    variant,
+    options,
+  })
   return <AnyA css={css as any} {...props} />
 }
