58debb0bb8d7bcf8ee720aade061139b634e3067
Jacco Meijer <jacco@jaccomeijer.nl>
2021-06-27 12:42:17 +0200

    chore: add codemod test

---
+++ packages/push/src/codemod.ts
@@ -0,0 +1,45 @@
+import ts, { JSDocTagInfo, SymbolDisplayPart } from 'typescript'
+import { sendToPlatform } from './send-to-platform'
+
+export interface DocProperty {
+  name?: string
+  documentationComment?: SymbolDisplayPart[]
+  jSDocTags?: JSDocTagInfo[]
+  type?: string
+  docProperties?: DocProperty[]
+}
+
+interface Visit {
+  node: ts.Node
+  printer: ts.Printer
+  checker: ts.TypeChecker
+  sourceFile: ts.SourceFile
+}
+const visit = ({ node, checker, printer, sourceFile }: Visit) => {
+  //   if (ts.isFunctionDeclaration(node)) {
+  //   }
+  const printThis = printer.printNode(ts.EmitHint.Unspecified, node, sourceFile)
+  console.log(printThis)
+}
+
+const generateDocumentation = () => {
+  const file = 'src/source2.tsx'
+  const program = ts.createProgram([file], {
+    target: ts.ScriptTarget.ES5,
+    module: ts.ModuleKind.CommonJS,
+  })
+  //   const sourceFile = program.getSourceFile(file)
+  const checker = program.getTypeChecker()
+  const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed })
+  const docProperties: DocProperty[] = []
+  for (const sourceFile of program.getSourceFiles()) {
+    if (!sourceFile.isDeclarationFile) {
+      ts.forEachChild(sourceFile, (node: ts.Node) => {
+        visit({ node, checker, printer, sourceFile })
+      })
+    }
+  }
+  sendToPlatform({ docProperties })
+}
+
+generateDocumentation()
--- packages/push/src/plain.ts
+++ packages/push/src/plain.ts
@@ -1,1 +1,1 @@
-export * from './push'
+export * from './codemod'
