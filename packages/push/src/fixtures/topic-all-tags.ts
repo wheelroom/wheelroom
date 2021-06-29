3cb4cfd81d6a47318ec0eb4aa97277fdaef9beb2
Jacco Meijer <jacco@jaccomeijer.nl>
2021-06-29 14:08:03 +0200

    refactor: move examples into fixtures

---
+++ packages/push/src/fixtures/topic-all-tags.ts
@@ -0,0 +1,28 @@
+/**
+ * Something about this model, used as model description
+ * @wheelroom {@platform contentful}
+ * @see https://www.wheelroom.io
+ */
+export interface Topic {
+  /**
+   * Some text here that we do nothing with
+   * @wheelroom
+   *   {@name Heading}
+   *   {@type Symbol}
+   *   {@widget singleLine}
+   *   {@required} {@localized}
+   *   {@validation unique}
+   *   {@helpText The topic heading}
+   * @defaultValue Default heading
+   *
+   */
+  heading: string
+  /**
+   * This is the abstract
+   */
+  abstract: string
+  /**
+   * This is the variant
+   */
+  variant: any
+}
