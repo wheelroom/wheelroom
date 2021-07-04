54c29d23a6eab091a87b093d96acff1cc5626c95
Jacco Meijer <jacco@jaccomeijer.nl>
2021-07-04 16:32:47 +0200

    chore: add errors

---
+++ packages/typescript-oauth2-server/src/error/oauth2-error.ts
@@ -0,0 +1,97 @@
+export enum HttpStatus {
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+}
+
+export enum OAuth2ErrorType {
+  AccessDenied = 'access_denied',
+  InvalidClient = 'invalid_client',
+  InvalidGrant = 'invalid_grant',
+  InvalidRequest = 'invalid_request',
+  InvalidScope = 'invalid_scope',
+  UnauthorizedClient = 'unauthorized_client',
+}
+
+class OAuth2Error extends Error {
+  constructor(
+    public message: string,
+    public description: string,
+    public status: HttpStatus,
+    public type: OAuth2ErrorType
+  ) {
+    super(message)
+    Object.setPrototypeOf(this, OAuth2Error.prototype)
+    this.name = this.constructor.name
+
+    if (typeof Error.captureStackTrace === 'function') {
+      Error.captureStackTrace(this, this.constructor)
+    } else {
+      this.stack = new Error(message).stack
+    }
+  }
+}
+
+export const accessDeniedErrorFactory = (description: string): OAuth2Error => {
+  const message = `Access denied`
+  return new OAuth2Error(
+    message,
+    description,
+    HttpStatus.UNAUTHORIZED,
+    OAuth2ErrorType.AccessDenied
+  )
+}
+
+export const invalidClientErrorFactory = (description: string): OAuth2Error => {
+  const message = `Unauthorized client, verification failed`
+  return new OAuth2Error(
+    message,
+    description,
+    HttpStatus.UNAUTHORIZED,
+    OAuth2ErrorType.InvalidClient
+  )
+}
+
+export const invalidGrantErrorFactory = (description: string): OAuth2Error => {
+  const message = `Invalid grant, supported grants are: authorization_code, client_credentials`
+  return new OAuth2Error(
+    message,
+    description,
+    HttpStatus.BAD_REQUEST,
+    OAuth2ErrorType.InvalidGrant
+  )
+}
+
+export const invalidRequestErrorFactory = (
+  arg: string,
+  description: string
+): OAuth2Error => {
+  const message = `Invalid request, caused by argument: ${arg})`
+  return new OAuth2Error(
+    message,
+    description,
+    HttpStatus.BAD_REQUEST,
+    OAuth2ErrorType.InvalidRequest
+  )
+}
+
+export const invalidScopeErrorFactory = (description: string): OAuth2Error => {
+  const message = `Invalid scope, a valid scope is required`
+  return new OAuth2Error(
+    message,
+    description,
+    HttpStatus.BAD_REQUEST,
+    OAuth2ErrorType.InvalidScope
+  )
+}
+
+export const unauthorizedClientErrorFactory = (
+  description: string
+): OAuth2Error => {
+  const message = `Unauthorized client, verification failed`
+  return new OAuth2Error(
+    message,
+    description,
+    HttpStatus.UNAUTHORIZED,
+    OAuth2ErrorType.UnauthorizedClient
+  )
+}
