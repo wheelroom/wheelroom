49319575a2f94a0bd10bd8ce2f7006ae036e3b94
Jacco Meijer <jacco@jaccomeijer.nl>
2021-07-04 22:06:53 +0200

    chore: update generator

--- packages/oauth2-response-generator/src/response-generator/finalize-authorize-response.ts
+++
@@ -1,9 +0,0 @@
-export interface FinalizeAuthorizeResponse {
-  req: Express.Request
-}
-
-export const finalizeAuthorizeResponse = ({
-  req,
-}: FinalizeAuthorizeResponse) => {
-  console.log('finalizeAuthorizeResponse, to Be Implemented', req)
-}
--- packages/oauth2-response-generator/src/response-generator/response-generator.ts
+++ packages/oauth2-response-generator/src/response-generator/response-generator.ts
@@ -1,18 +1,28 @@
+import Express from 'express'
 import { authorizeResponse } from './authorize-response'
-import { finalizeAuthorizeResponse } from './finalize-authorize-response'
 import { tokenResponse } from './token-response'
 
 export interface ResponseGenerator {
-  action: 'authorize' | 'finalizeAuthorize' | 'token'
+  action: 'authorize' | 'token'
   req: Express.Request
 }
 
 export const responseGenerator = ({ action, req }: ResponseGenerator) => {
+  const urlObj = new URL(req.headers.location || '')
+  const responeType = urlObj.searchParams.get('response_type')
+  const grantType = urlObj.searchParams.get('grant_type')
+
+  // response_type (authorize endpoint)
+  // - code
+  //
+  // grant_type (token endpoint)
+  // - authorization_code
+  // - refresh_token
+  // - client_credentials
+
   switch (action) {
     case 'authorize':
       return authorizeResponse({ req })
-    case 'finalizeAuthorize':
-      return finalizeAuthorizeResponse({ req })
     case 'token':
       return tokenResponse({ req })
 
