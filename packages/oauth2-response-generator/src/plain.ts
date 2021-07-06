283a05c0d7aecc732d74c311f746863890212bb1
Jacco Meijer <jacco@jaccomeijer.nl>
2021-07-06 14:46:59 +0200

    chore: fix build

--- packages/oauth2-response-generator/src/plain.ts
+++ packages/oauth2-response-generator/src/plain.ts
@@ -1,1 +1,18 @@
-export * from './server/server'
+export * from './collection/auth-code'
+export * from './collection/client'
+export * from './collection/collection-api'
+export * from './collection/scope'
+export * from './collection/token'
+export * from './collection/user'
+
+export * from './error/oauth2-error'
+
+export * from './jwt/access-token'
+export * from './jwt/code-token'
+export * from './jwt/id-token'
+export * from './jwt/jwt'
+export * from './jwt/jwt-api'
+export * from './jwt/refresh-token'
+
+export * from './response/response'
+export * from './response/max-age'
